cded6db1f0b4b867278861570150fd4046e145de
FlynnLee <593029372@qq.com>
2019-02-22 09:47:29 +0800

    add 404

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,10 +3,13 @@ import { authService } from '@/services/auth.service'
 import { userService } from '@/services/user.service'
 import { createRoutesFromStRoutes } from './generate'
 import generatedRoutes from './generated-routes'
-console.log(generatedRoutes)
 
 const stRoutes: StRouteConfig[] = [
-
+  ...generatedRoutes,
+  {
+    path: '*',
+    redirect: '404'
+  }
 ]
 
-export const routes = createRoutesFromStRoutes(generatedRoutes)
+export const routes = createRoutesFromStRoutes(stRoutes)
