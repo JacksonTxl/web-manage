f0892928fbdacfe9f4e3cd05d1eadd30bfbbeb05
zhenglinxiong <zlx1433@gmail.com>
2019-03-04 09:27:20 +0800

    create route

--- src/router/generate-page-routes.js
+++ src/router/generate-page-routes.js
@@ -4,11 +4,13 @@
 
 import { dashboardService } from '@/views/pages/dashboard.service.ts'
 import { llfService } from '@/views/pages/test/llf.service.ts'
+import { zlxService } from '@/views/pages/test/zlx.service.ts'
 import { loginService } from '@/views/pages/user/login.service.ts'
 
 const page404 = () => import('@/views/pages/404.vue')
 const pageDashboard = () => import('@/views/pages/dashboard.vue')
 const pageTestLlf = () => import('@/views/pages/test/llf.vue')
+const pageTestZlx = () => import('@/views/pages/test/zlx.vue')
 const pageUserLogin = () => import('@/views/pages/user/login.vue')
 
 // service
@@ -27,6 +29,11 @@ const pageRoutes = [{
   path: '/test/llf',
   component: pageTestLlf,
   guards: [llfService]
+}, {
+  name: 'test-zlx',
+  path: '/test/zlx',
+  component: pageTestZlx,
+  guards: [zlxService]
 }, {
   name: 'user-login',
   path: '/user/login',
