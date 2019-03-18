9eaea2737f3fdd1d2d4bcf4a97e2ef74a01fc3c1
zhenglinxiong <zlx1433@gmail.com>
2019-03-18 18:32:21 +0800

    hot-release

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp')
 const Fse = require('fs-extra')
 const LessTask = require('./build/less-task')
+const RouteTask = require('./build/route-task')
+const chalk = require('chalk')
 
 const lessSrc = './src/style/index.less'
 const lessDest = './public/app.less'
@@ -13,7 +15,18 @@ gulp.task('less', done => {
 })
 
 gulp.task('route', done => {
+  RouteTask.run('init', 'init')
   done()
 })
 
 gulp.watch(['./src/**/*.less'], gulp.series(['less']))
+gulp.watch(['./src/views/pages/**/*.vue'], gulp.series(['route']))
+  .on('add', path => {
+    RouteTask.run(path, '  add  ')
+  })
+  .on('unlink', path => {
+    RouteTask.run(path, 'remove file')
+  })
+  .on('unlinkDir', path => {
+    RouteTask.run(path, 'remove Dir')
+  })
