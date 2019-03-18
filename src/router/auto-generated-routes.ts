03b4bf9d0469f30ccd621921299edd7eb7363d11
FlynnLee <593029372@qq.com>
2019-03-18 15:58:05 +0800

    fix BASE_URL

--- src/router/auto-generated-routes.ts
+++ src/router/auto-generated-routes.ts
@@ -6,9 +6,6 @@ import { DashboardService } from '@/views/pages/dashboard.service.ts'
 import { PluginsService } from '@/views/pages/plugins.service.ts'
 import { ListService as PluginsListService } from '@/views/pages/plugins/list.service.ts'
 import { ZlxService as TestZlxService } from '@/views/pages/test/zlx.service.ts'
-import { RouterService as TestTestRouterRouterService } from '@/views/pages/test/testRouter/router.service.ts'
-import { Router2Service as TestTestRouterRouter2Service } from '@/views/pages/test/testRouter/router2.service.ts'
-import { ListService as TestTestRouterDeeprListService } from '@/views/pages/test/testRouter/deepr/list.service.ts'
 
 const page404 = () => import('@/views/pages/404.vue')
 const pageDashboard = () => import('@/views/pages/dashboard.vue')
@@ -19,15 +16,6 @@ const pageTestLlf = () => import('@/views/pages/test/llf.vue')
 const pageTestZlx = () => import('@/views/pages/test/zlx.vue')
 const pageTestZlxFull = () => import('@/views/pages/test/zlxFull.vue')
 const pageUserLogin = () => import('@/views/pages/user/login.vue')
-const pageTestTestRouterRouter = () => import('@/views/pages/test/testRouter/router.vue')
-const pageTestTestRouterRouter2 = () => import('@/views/pages/test/testRouter/router2.vue')
-const pageTestTestRouterDeeprList = () => import('@/views/pages/test/testRouter/deepr/list.vue')
-const pageTestTestRouterRouterA = () => import('@/views/pages/test/testRouter/router/a.vue')
-const pageTestTestRouterRouterB = () => import('@/views/pages/test/testRouter/router/b.vue')
-const pageTestTestRouterRouter2A = () => import('@/views/pages/test/testRouter/router2/a.vue')
-const pageTestTestRouterRouter2C = () => import('@/views/pages/test/testRouter/router2/c.vue')
-const pageTestTestRouterRouterAAsub = () => import('@/views/pages/test/testRouter/router/a/asub.vue')
-const pageTestTestRouterRouterAAsub1 = () => import('@/views/pages/test/testRouter/router/a/asub1.vue')
 
 // service
 const pageRoutes = [{
@@ -50,6 +38,11 @@ const pageRoutes = [{
     path: 'list',
     component: pagePluginsList,
     guards: [PluginsListService]
+  }, {
+    name: 'plugins-theme',
+    path: 'theme',
+    component: pagePluginsTheme,
+    guards: []
   }]
 }, {
   name: 'test-llf',
@@ -71,54 +64,6 @@ const pageRoutes = [{
   path: '/user/login',
   component: pageUserLogin,
   guards: []
-}, {
-  name: 'test-testRouter-router',
-  path: '/test/testRouter/router',
-  component: pageTestTestRouterRouter,
-  guards: [TestTestRouterRouterService],
-  children: [{
-    name: 'test-testRouter-router-a',
-    path: 'a',
-    component: pageTestTestRouterRouterA,
-    guards: [],
-    children: [{
-      name: 'test-testRouter-router-a-asub',
-      path: 'asub',
-      component: pageTestTestRouterRouterAAsub,
-      guards: []
-    }, {
-      name: 'test-testRouter-router-a-asub1',
-      path: 'asub1',
-      component: pageTestTestRouterRouterAAsub1,
-      guards: []
-    }]
-  }, {
-    name: 'test-testRouter-router-b',
-    path: 'b',
-    component: pageTestTestRouterRouterB,
-    guards: []
-  }]
-}, {
-  name: 'test-testRouter-router2',
-  path: '/test/testRouter/router2',
-  component: pageTestTestRouterRouter2,
-  guards: [TestTestRouterRouter2Service],
-  children: [{
-    name: 'test-testRouter-router2-a',
-    path: 'a',
-    component: pageTestTestRouterRouter2A,
-    guards: []
-  }, {
-    name: 'test-testRouter-router2-c',
-    path: 'c',
-    component: pageTestTestRouterRouter2C,
-    guards: []
-  }]
-}, {
-  name: 'test-testRouter-deepr-list',
-  path: '/test/testRouter/deepr/list',
-  component: pageTestTestRouterDeeprList,
-  guards: [TestTestRouterDeeprListService]
 }]
 
 export default pageRoutes
