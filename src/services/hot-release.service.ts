74a38b17d47011981efd65ae27bb615af05af69b
zhenglinxiong <zlx1433@gmail.com>
2019-03-18 16:38:13 +0800

    hot-release

--- src/services/hot-release.service.ts
+++ src/services/hot-release.service.ts
@@ -13,21 +13,22 @@ export class HotReleaseService implements RouteGuard {
       debounceTime(1000),
       // 捕获子流的错误 错误冒泡会导致父流中断
       switchMap(() =>
-        ajax.get('/release.json?123').pipe(catchError(() => EMPTY))
+        ajax.get(`/release.json?${new Date().getTime()}`).pipe(catchError(() => EMPTY))
       ),
-      tap((response: any) => {
+      tap(({ response }) => {
         if (response.git_commit !== this.appConfig.GIT_COMMIT) {
           console.log(
             '需要刷新页面',
             'GIT_COMMIT：',
             this.appConfig.GIT_COMMIT,
+            response.git_commit,
             'release.commit：'
           )
+          console.log('延迟更新成功')
           if (this.appConfig.IS_PROD) {
             document.location.reload()
           }
         }
-        console.log('延迟更新成功')
       })
     )
   )
--- src/views/pages/test/zlx.vue
+++ src/views/pages/test/zlx.vue
@@ -1,8 +1,5 @@
 <template>
   <div>
-    <p>
-      Here is the demo of vue-fullcalendar, no jquery fullcalendar.j
-    </p>
     <full-calendar class="test-fc"
     :events="fcEvents"
     first-day='1'
