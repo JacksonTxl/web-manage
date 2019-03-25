a9334c6cd8715a94dfbcd22a3cfdf17e5c6f1266
FlynnLee <593029372@qq.com>
2019-03-25 16:58:14 +0800

    no antd less

--- src/views/layouts/default.vue
+++ src/views/layouts/default.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="layout-default">
-    <router-view></router-view>
-  </div>
+  <a-layout class="layout-default">
+    <a-layout-sider>
+      hello
+
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header>
+
+      </a-layout-header>
+      <a-layout-content>
+        <div class="content">
+          <router-view></router-view>
+        </div>
+      </a-layout-content>
+      <a-layout-footer>
+        Styd ©2019 Created
+      </a-layout-footer>
+
+    </a-layout>
+
+  </a-layout>
 </template>
