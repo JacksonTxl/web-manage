8f23571a26e78da4b3dc8b8ced511ac1214b8639
卡尔 <yupeng@styd.cn>
2019-03-27 10:46:59 +0800

    layout demo

--- src/views/layouts/index.ts
+++ src/views/layouts/index.ts
@@ -1,12 +1,13 @@
 import LayoutBlank from './blank.vue'
 import LayoutDefault from './default.vue'
+import LayoutDefaultBrand from './kael-default-brand.vue'
 import LayoutLogin from './login.vue'
 import LayoutLoading from './loading.vue'
 
 // this is will used in router/routes
 export const layoutMap = {
   blank: LayoutBlank,
-  'default-brand': LayoutDefault,
+  'default-brand': LayoutDefaultBrand,
   'default-shop': LayoutDefault,
   login: LayoutLogin,
   loading: LayoutLoading
