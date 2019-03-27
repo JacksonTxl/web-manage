c8c653b1529c2671a91342af44a2b3039b27e69e
卡尔 <yupeng@styd.cn>
2019-03-27 11:45:03 +0800

    checkbox-button

--- src/views/components/register.ts
+++ src/views/components/register.ts
@@ -1,8 +1,8 @@
 // global register components 全局注册的组件
 import Vue from 'vue'
 import StIcon from './st-icon.vue'
-import StCheckboxButtonGroup from './st-checkbox-button-group.vue'
-import StCheckboxButtonItem from './st-checkbox-button-item.vue'
+import StCheckboxButtonGroup from './checkbox-button/checkbox-button-group.vue'
+import StCheckboxButtonItem from './checkbox-button/checkbox-button-item.vue'
 
 Vue.component(StIcon.name, StIcon)
 Vue.component(StCheckboxButtonGroup.name, StCheckboxButtonGroup)
