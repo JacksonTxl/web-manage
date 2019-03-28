7fcc9b67a97ba9c5fa2d0c57ed7165ddd6aacd61
zhenglinxiong <zlx1433@gmail.com>
2019-03-28 09:17:14 +0800

    bugfix gen router

--- src/views/pages/staff/components/opreation-button.vue
+++ src/views/pages/staff/components/opreation-button.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="opreation-button">
-    <modal-link tag="a-button"  :to="{name: 'add-staff'}" class="opreation-button__modal" type="primary" ghost>添加员工</modal-link>
+    <st-button  class="opreation-button__modal" type="primary" @click="onClickAddStaff" ghost>添加员工</st-button>
     <st-button class="opreation-button__modal">批量导入员工</st-button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'OpreationButton'
+  name: 'OpreationButton',
+  methods: {
+    onClickAddStaff() {
+      this.$route.push('')
+    }
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
