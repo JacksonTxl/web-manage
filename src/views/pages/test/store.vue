5fddd3f43117337233caa68f3ed27638e372f3d1
崔庆 <cq_weifeng@163.com>
2019-03-28 20:45:43 +0800

    stotr style

--- src/views/pages/test/store.vue
+++ src/views/pages/test/store.vue
@@ -1,30 +1,37 @@
 <template>
-  <div class="pages-test-store">
-    <a-row>
-      <a-col :span="24">
-        <div class="pages-test-store__img-box">
-          <img
-            class="pages-test-store__sign-img pages-test-store__img"
-            src="https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=234634259,4236876085&fm=27&gp=0.jpg"
-            alt
-          >
-          <img
-            class="pages-test-store__bg-img pages-test-store__img"
-            src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1986179278,1118313821&fm=27&gp=0.jpg"
-            alt
-          >
-          <span class="pages-test-store__sign-icon">店招</span>
+  <div class="pages-test-store container-basis">
+    <div class="pages-test-store__container">
+      <div class="pages-test-store__img-box">
+        <img class="pages-test-store__sign-img pages-test-store__img" :src="signUrl" alt>
+        <img class="pages-test-store__bg-img pages-test-store__img" :src="bgUrl" alt>
+        <span class="pages-test-store__sign-icon">店招</span>
+      </div>
+      <div class="pages-test-store__info">
+        <div class="pages-test-store__info-title">
+          <span>营业中</span>
+          <h2>上海旗舰店</h2>
         </div>
-      </a-col>
-      <a-col :span="24">
-        <div>营业时间</div>
-        <slider class="pages-test-store__slider"></slider>
-        <slider class="pages-test-store__slider"></slider>
-        <slider class="pages-test-store__slider"></slider>
-        <slider class="pages-test-store__slider"></slider>
-        <slider class="pages-test-store__slider"></slider>
-      </a-col>
-    </a-row>
+        <div class="pages-test-store__info-list">
+          <span>门店电话</span>
+          <span>1234568909753246</span>
+        </div>
+        <div class="pages-test-store__info-list">
+          <span>门店电话</span>
+          <span>1234568909753246</span>
+        </div>
+        <div class="pages-test-store__info-list">
+          <span>门店电话</span>
+          <span>1234568909753246</span>
+        </div>
+      </div>
+    </div>
+
+    <div>营业时间</div>
+    <slider class="pages-test-store__slider"></slider>
+    <slider class="pages-test-store__slider"></slider>
+    <slider class="pages-test-store__slider"></slider>
+    <slider class="pages-test-store__slider"></slider>
+    <slider class="pages-test-store__slider"></slider>
   </div>
 </template>
 
@@ -36,7 +43,11 @@ export default {
   },
   data() {
     return {
-      disabled: true
+      disabled: true,
+      signUrl:
+        'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=234634259,4236876085&fm=27&gp=0.jpg',
+      bgUrl:
+        'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1986179278,1118313821&fm=27&gp=0.jpg'
     }
   },
   methods: {
