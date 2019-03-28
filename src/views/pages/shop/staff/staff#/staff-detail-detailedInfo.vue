ce76b6cc6c93751f836004fc7f7c69fd42fccea8
zhenglinxiong <zlx1433@gmail.com>
2019-03-28 16:21:53 +0800

    git dir

---
+++ src/views/pages/shop/staff/staff#/staff-detail-detailedInfo.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        详细信息
+        <a-button @click="save">保存</a-button>
+        <a-button type="primary" @click="goNext">继续，填写</a-button>
+    </div>
+</template>
+<script>
+export default {
+  name: 'StaffDetailDetailedInfo',
+  methods: {
+    goNext() {
+      let data = {
+        isGoNext: true
+      }
+      this.$emit('goNext', data)
+    },
+    save() {
+
+    }
+  }
+}
+</script>
