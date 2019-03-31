b2ae57cd411391706f1e8bba11e57b9462029a76
zhenglinxiong <zlx1433@gmail.com>
2019-03-31 17:36:01 +0800

    git dir

---
+++ src/views/modals/staff/turnover.vue
@@ -0,0 +1,27 @@
+<template>
+  <a-modal title='员工离职'
+    confirmLoading
+    @ok='onSubmit'
+    v-model='show'>
+    <section>
+      <st-tag type="coach-personal"/>
+      <st-tag type="coach-team"/>
+      <st-tag type="role-staff"/>
+    </section>
+  </a-modal>
+</template>
+<script>
+export default {
+  name: 'AddStaffModal',
+  data() {
+    return {
+      show: false
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log('ok')
+    }
+  }
+}
+</script>
