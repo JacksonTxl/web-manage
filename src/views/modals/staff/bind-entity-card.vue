f04f11626aab6467ca1270adea1a463a5dbb26cf
zhenglinxiong <zlx1433@gmail.com>
2019-04-01 12:40:50 +0800

    git dir

---
+++ src/views/modals/staff/bind-entity-card.vue
@@ -0,0 +1,29 @@
+<template>
+  <a-modal title='绑定实体卡'
+    confirmLoading
+    @ok='onSubmit'
+    v-model='show'>
+    <section>
+      <div class="staff-tag">
+        <st-tag class="mg-r4" type="coach-personal"/>
+        <st-tag class="mg-r4" type="coach-team"/>
+        <st-tag class="mg-r8" type="role-staff"/>
+      </div>
+    </section>
+  </a-modal>
+</template>
+<script>
+export default {
+  name: 'BindEntityCard',
+  data() {
+    return {
+      show: false
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log('ok')
+    }
+  }
+}
+</script>
