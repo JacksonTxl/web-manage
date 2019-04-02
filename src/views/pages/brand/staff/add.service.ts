095dd9733000e15296b255de58956689086c7ce6
zhenglinxiong <zlx1433@gmail.com>
2019-04-02 16:33:41 +0800

    git dir

--- src/views/pages/brand/staff/add.service.ts
+++ src/views/pages/brand/staff/add.service.ts
@@ -1,19 +1,20 @@
 import { SaveData } from './../../../../api/staff/staff-add'
 import { Injectable, ServiceRoute, RouteGuard } from 'vue-service-app'
 import { StaffAddApi } from '@/api/staff/staff-add'
+import { tap } from 'rxjs/operators'
 import { StaffPubApi } from '@/api/staff/staff-pub'
 
 @Injectable()
 export class AddService {
-  constructor(private StaffPubApi: StaffPubApi, private StaffAddApi: StaffAddApi) {
+  constructor(private staffPubApi: StaffPubApi, private staffAddApi: StaffAddApi) {
 
   }
   getCounty() { // 获取国际化手机号前缀
-    return this.StaffPubApi.getCountry()
+    return this.staffPubApi.getCountry()
   }
 
   save(data: SaveData) {
-    return this.StaffAddApi.saveStaffInfo(data)
+    return this.staffAddApi.saveStaffInfo(data)
   }
 
   beforeRouteEach(to: ServiceRoute, from: ServiceRoute, next: any) {
