b42a429321c59e673ec0a92b3f02fa924d4f82a9
FlynnLee <593029372@qq.com>
2019-04-08 11:13:56 +0800

    add modal example

--- src/views/pages/styleguide/modal.vue
+++ src/views/pages/styleguide/modal.vue
@@ -11,15 +11,15 @@
     <p>这里的模态窗名称同modals文件夹下的模态窗路径</p>
     <section class="sg-app-content">
       <st-panel>
-        <modal-link tag="st-button" :to="{ name: 'test-sg',on:{ok:onOk} }">test-sg</modal-link>
+        <modal-link tag="st-button" :to="{ name: 'test-sg',on:{ok:onOk} }">使用组件test-sg</modal-link>
+        <button class="mg-l16" @click="onCustomClick">使用api</button>
       </st-panel>
     </section>
 
-    <p>手动触发模态窗弹出</p>
+    <h3>常用业务模态窗</h3>
 
     <section class="sg-app-content">
       <st-panel>
-        <button @click="onCustomClick">手动弹出莫泰窗</button>
       </st-panel>
     </section>
   </div>
