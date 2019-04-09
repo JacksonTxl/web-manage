af8a5c8ff9da9ab04194fd8c63e7e86a9579e703
崔庆 <cq_weifeng@163.com>
2019-04-09 19:00:59 +0800

    slider

---
+++ src/views/components/slider/slider.1.vue
@@ -0,0 +1,332 @@
+<template>
+  <div>
+    <st-checkbox-button-group v-model="weekArr">
+      <st-checkbox-button-item
+        v-for="item in weekList"
+        :key="item.value"
+        :value="item.value"
+      >{{item.label}}</st-checkbox-button-item>
+    </st-checkbox-button-group>
+    <div class="st-slider st-slider-box">
+      <a-row class="st-slider__title-box">
+        <a-col :span="2">时间段</a-col>
+        <a-col :span="18">
+          <ul class="st-slider__title-box-time">
+            <li class="st-slider__title-box-time-comment">00:00</li>
+            <li class="st-slider__title-box-time-06 st-slider__title-box-time-comment">06:00</li>
+            <li class="st-slider__title-box-time-12 st-slider__title-box-time-comment">12:00</li>
+            <li class="st-slider__title-box-time-18 st-slider__title-box-time-comment">18:00</li>
+            <li class="st-slider__title-box-time-24 st-slider__title-box-time-comment">24:00</li>
+          </ul>
+        </a-col>
+        <a-col :span="4" class="st-slider__title-box-operate">操作</a-col>
+      </a-row>
+      <div class="slider" v-for="(item,index) in setSlider.infoList" :key="index">
+        <a-row>
+          <a-col :span="2">
+            <span class="slider__title">{{setSlider.infoList[index].title}}</span>
+          </a-col>
+          <a-col :span="18">
+            <a-slider
+              range
+              :min="0"
+              :max="24"
+              :step="0.5"
+              :defaultValue="setSlider.infoList[index].value"
+              :tipFormatter="formatter"
+              :tooltipVisible="true"
+              @change="sliderFunc"
+              @afterChange="onAfterChange(item,index)"
+              v-model="setSlider.infoList[index].value"
+            />
+          </a-col>
+          <a-col :span="4">
+            <a-popover placement="bottomRight" trigger="click" class="slider-copy-bottom">
+              <template slot="content">
+                <a-checkbox-group
+                  @change="onChange"
+                  class="slider-copy"
+                  v-for=" (weekInfo,index) in item.week"
+                  :key="index"
+                >
+                  <a-checkbox :value="index" :disabled="weekInfo.disabled">{{weekInfo.key}}</a-checkbox>
+                </a-checkbox-group>
+              </template>
+              <span @click="copyTo(index)">复制到</span>
+            </a-popover>
+          </a-col>
+        </a-row>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { constant } from 'lodash-es'
+export default {
+  name: 'StSlider',
+  props: {
+    getSlider: {
+      type: Object
+    }
+  },
+  data() {
+    return {
+      // 星期选择
+      weekArr: [],
+      // 保留
+      weekArrRetain: [],
+      weekList: [
+        { value: 1, label: '周一' },
+        { value: 2, label: '周二' },
+        { value: 3, label: '周三' },
+        { value: 4, label: '周四' },
+        { value: 5, label: '周五' },
+        { value: 6, label: '周六' },
+        { value: 7, label: '周日' }
+      ],
+      setSlider: [],
+      // 获取的滑动的slider数值
+      getSliderArr: [],
+      // 默认
+      defaultWeekList: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+      getWeek: [],
+      // 获取点击的是哪一个slider
+      copyIndex: 0
+    }
+  },
+  mounted() {
+    // 初始话数据
+    this.setSlider = this.getFilterSlider(this.getSlider)
+  },
+  methods: {
+    copyTo(copyIndex) {
+      this.copyIndex = copyIndex
+    },
+    onChange(data) {
+      let self = this
+      let toCopy = self.defaultWeekList[data]
+      self.setSlider.infoList.forEach(item => {
+        if (item.title === toCopy) {
+          item.value = self.setSlider.infoList[self.copyIndex].value
+          self.setSlider = JSON.parse(JSON.stringify(self.setSlider))
+          self.$forceUpdate()
+        }
+      })
+    },
+    // 数据改变后改变data数据
+    onAfterChange(data, index) {
+      if (this.getSliderArr.length > 0) {
+        data.value = this.getSliderArr
+        this.setSlider.infoList[index] = data
+        this.getSliderArr = []
+      }
+    },
+    // 获取改变后的slider数据
+    sliderFunc(data) {
+      this.getSliderArr = data
+      // console.log('sliderFunc', data)
+    },
+    // 获取的数据对格式进行处理
+    getFilterSlider(getSliderData) {
+      let self = this
+      let weekArr = []
+      let getSlider = JSON.parse(JSON.stringify(getSliderData))
+      if (getSlider.business_time) {
+        let filterSlider = []
+        getSlider.business_time.map(function(item) {
+          let week_day =
+            item.week_day === 1
+              ? '周一'
+              : item.week_day === 2
+                ? '周二'
+                : item.week_day === 3
+                  ? '周三'
+                  : item.week_day === 4
+                    ? '周四'
+                    : item.week_day === 5
+                      ? '周五'
+                      : item.week_day === 6
+                        ? '周六'
+                        : '周日'
+          weekArr.push(item.week_day)
+          filterSlider.push({
+            title: week_day,
+            value: [
+              item.start_time.replace(/:00/gi, '').replace(/:30/gi, '.5') - 0,
+              item.end_time.replace(/:00/gi, '').replace(/:30/gi, '.5') - 0
+            ],
+            week: []
+          })
+          // console.log(item.week_day)
+          self.getWeek.push(week_day)
+        })
+        self.weekArr = weekArr
+        self.weekArrRetain = JSON.parse(JSON.stringify(weekArr))
+        getSlider.infoList = filterSlider
+        return self.copyWeek(getSlider)
+      }
+    },
+    getFilterSlider1(getSliderData) {
+      let self = this
+      let getSlider = JSON.parse(JSON.stringify(getSliderData))
+      if (getSlider.business_time) {
+        let filterSlider = []
+        getSlider.business_time.map(function(item) {
+          let week_day =
+            item.week_day === 1
+              ? '周一'
+              : item.week_day === 2
+                ? '周二'
+                : item.week_day === 3
+                  ? '周三'
+                  : item.week_day === 4
+                    ? '周四'
+                    : item.week_day === 5
+                      ? '周五'
+                      : item.week_day === 6
+                        ? '周六'
+                        : '周日'
+          filterSlider.push({
+            title: week_day,
+            value: [
+              item.start_time.replace(/:00/gi, '').replace(/:30/gi, '.5') - 0,
+              item.end_time.replace(/:00/gi, '').replace(/:30/gi, '.5') - 0
+            ],
+            week: []
+          })
+          // console.log(item.week_day)
+          self.getWeek.push(week_day)
+        })
+        getSlider.infoList = filterSlider
+        return self.copyWeek(getSlider)
+      }
+    },
+    // 对复制日期进行处理
+    copyWeek(getSlider) {
+      let self = this
+      let addcopyWeek = JSON.parse(JSON.stringify(getSlider))
+      addcopyWeek.infoList.forEach((item, index) => {
+        let w = []
+        for (let i = 0; i < 7; i++) {
+          w.push({
+            key: self.defaultWeekList[i],
+            disabled:
+              !(self.getWeek.indexOf(self.defaultWeekList[i]) >= 0 &&
+              item.title !== self.defaultWeekList[i])
+          })
+        }
+        item.week = w
+      })
+      return addcopyWeek
+    },
+    // tooltip格式处理
+    formatter(value) {
+      if (value % 1 === 0) {
+        if (value > 9) {
+          return `${value}:00`
+        } else {
+          return `0${value}:00`
+        }
+      } else {
+        if (value > 9) {
+          return `${parseInt(value)}:30`
+        } else {
+          return `0${parseInt(value)}:30`
+        }
+      }
+    },
+    // 还原格式
+    setFilterSlider() {
+      let self = this
+      let setSlider = JSON.parse(JSON.stringify(this.setSlider))
+      let filterData = []
+      setSlider.infoList.map(function(item) {
+        filterData.push(self.filterSlider(item))
+      })
+      setSlider.infoList = filterData
+      return setSlider
+    },
+    filterSlider(item) {
+      return {
+        week_day:
+          item.title === '周一'
+            ? 1
+            : item.title === '周二'
+              ? 2
+              : item.title === '周三'
+                ? 3
+                : item.title === '周四'
+                  ? 4
+                  : item.title === '周五'
+                    ? 5
+                    : item.title === '周六'
+                      ? 6
+                      : 7,
+        start_time:
+          /^\d+$/.test(item.value[0]) && item.value[0] > 9
+            ? item.value[0] + ':00'
+            : /^\d+$/.test(item.value[0]) && item.value[0] <= 9
+              ? '0' + item.value[0] + ':00'
+              : !/^\d+$/.test(item.value[0]) && item.value[0] <= 9
+                ? '0' + (item.value[0] + '').replace(/.5/gi, ':30')
+                : (item.value[0] + '').replace(/.5/gi, ':30') + '',
+        end_time:
+          /^\d+$/.test(item.value[1]) && item.value[1] > 9
+            ? item.value[1] + ':00'
+            : /^\d+$/.test(item.value[1]) && item.value[1] <= 9
+              ? '0' + item.value[1] + ':00'
+              : !/^\d+$/.test(item.value[1]) && item.value[1] <= 9
+                ? '0' + (item.value[1] + '').replace(/.5/gi, ':30')
+                : (item.value[1] + '').replace(/.5/gi, ':30') + ''
+      }
+    },
+    getArrDifference(arr1, arr2) {
+      return arr1.concat(arr2).filter(function(v, i, arr) {
+        return arr.indexOf(v) === arr.lastIndexOf(v)
+      })
+    }
+  },
+  created() {},
+  watch: {
+    setSlider: {
+      handler() {
+        console.log(this.setFilterSlider().infoList)
+        this.$emit('change', this.setFilterSlider().infoList)
+      },
+      deep: true
+    },
+    weekArr: {
+      handler() {
+        let self = this
+        let weekArr = JSON.parse(JSON.stringify(this.weekArr))
+        weekArr.sort()
+        if (weekArr.length > self.weekArrRetain.length) {
+          if (self.getArrDifference(weekArr, self.weekArrRetain).length > 0) {
+            self.setSlider.business_time.push({
+              week_day: self.getArrDifference(weekArr, self.weekArrRetain)[0],
+              start_time: '09:00',
+              end_time: '18:00'
+            })
+          }
+        } else {
+          if (self.getArrDifference(weekArr, self.weekArrRetain).length > 0) {
+            self.setSlider.business_time.forEach((item, index) => {
+              if (
+                self.getArrDifference(weekArr, self.weekArrRetain)[0] ===
+                item.week_day
+              ) {
+                self.setSlider.business_time.splice(index, 1)
+              }
+            })
+          }
+        }
+        self.weekArrRetain = weekArr
+        self.setSlider.business_time.sort((a, b) => {
+          return a.week_day - b.week_day
+        })
+        self.setSlider = self.getFilterSlider1(self.setSlider)
+      }
+    }
+  }
+}
+</script>
--- src/views/pages/test/store.vue
+++ src/views/pages/test/store.vue
@@ -1,186 +1,36 @@
 <template>
   <div class="pages-test-store container-basis">
-    <div class="pages-test-store__container">
-      <div class="pages-test-store__img-box">
-        <img class="pages-test-store__sign-img pages-test-store__img" :src="signUrl" alt>
-        <!-- <img class="pages-test-store__bg-img pages-test-store__img" :src="bgUrl" alt> -->
-        <span class="pages-test-store__sign-icon">
-          <span>
-            <st-icon type="home"></st-icon>
-          </span>
-          666
-        </span>
-      </div>
-      <div class="pages-test-store__info">
-        <div class="pages-test-store__info-title">
-          <span>营业中 {{data.shop_status === 1 ? '试运营' : '试运营'}}</span>
-          <h2>上海旗舰店</h2>
-        </div>
-        <div class="pages-test-store__info-list">
-          <span>门店电话</span>
-          <span>400-888999, 182520899999</span>
-        </div>
-        <div class="pages-test-store__info-list">
-          <span>门店邮箱</span>
-          <span>hurong@styd.cn</span>
-        </div>
-        <div class="pages-test-store__info-list">
-          <span>门店地址</span>
-          <span>上海市 徐汇区 徐汇区中山南二路107号抽换大厦3楼</span>
-          <a>查看定位</a>
-        </div>
-        <div class="pages-test-store__info-list">
-          <span class="pages-test-store__info-facilities">门店设施</span>
-          <div class="pages-test-store__info-facilities-list">
-            <div>
-              <st-icon type="home"></st-icon>
-              <p>Wi-Fi</p>
-            </div>
-            <div>
-              <st-icon type="home"></st-icon>
-              <p>空调</p>
-            </div>
-            <div>
-              <st-icon type="home"></st-icon>
-              <p>充电宝</p>
-            </div>
-            <div>
-              <st-icon type="home"></st-icon>
-              <p>无烟区</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="pages-test-store__time">营业时间</div>
     <slider class="pages-test-store__slider" :getSlider="getSlider"></slider>
   </div>
 </template>
 
 <script>
-import slider from '../../components/slider/slider.vue'
+import slider from '../../components/slider/slider.1.vue'
 export default {
   components: {
     slider
   },
   data() {
     return {
-      sliderArr: [],
-      data: {
-        'shop_name': '更新测试',
-        'shop_status': 0,
-        'lat': 2.374767,
-        'lng': 6.24567,
-        'shop_position': {
-          'province_id': 1,
-          'city_id': 2,
-          'district_id': 810012,
-          'address': '越界创意园',
-          'province_name': '香港特别行政区',
-          'city_name': '香港特别行政区',
-          'district_name': '元朗区',
-          'address_detail': '香港特别行政区香港特别行政区元朗区越界创意园'
-        },
-        'email': 'test@gmail.com',
-        'shop_phones': [
-          '13900000001',
-          '13800000001'
-        ],
-        'business_time': [
-          {
-            'week_day': 2,
-            'start_time': '10:00',
-            'end_time': '24:00'
-          },
-          {
-            'week_day': 5,
-            'start_time': '22:00',
-            'end_time': '24:00'
-          }
-        ],
-        'shop_services': [
-          {
-            'service_id': 2,
-            'service_name': '停车场',
-            'img_url': ''
-          },
-          {
-            'service_id': 3,
-            'service_name': '桑拿房',
-            'img_url': 'xxxxxx'
-          }
-        ]
-      },
       getSlider: {
-        className: 'st-slider-box',
-        disabled: true,
-        infoList: [
+        business_time: [
           {
-            title: '周一',
-            value: [0.5, 13]
-
+            week_day: 1,
+            start_time: '00:00',
+            end_time: '24:00'
           },
           {
-            title: '周二',
-            value: [6, 13]
-
+            week_day: 2,
+            start_time: '10:00',
+            end_time: '24:00'
           },
           {
-            title: '周三',
-            value: [12, 13.5]
-          },
-          {
-            title: '周四',
-            value: [18, 24]
+            week_day: 5,
+            start_time: '22:00',
+            end_time: '24:00'
           }
-          // {
-          //   title: '周五',
-          //   value: [9, 13]
-          // },
-          // {
-          //   title: '周六',
-          //   value: [5, 13]
-          //   // week: [
-          //   //   {
-          //   //     key: '周一',
-          //   //     disabled: false
-          //   //   },
-          //   //   {
-          //   //     key: '周二',
-          //   //     disabled: false
-          //   //   },
-          //   //   {
-          //   //     key: '周三',
-          //   //     disabled: true
-          //   //   },
-          //   //   {
-          //   //     key: '周四',
-          //   //     disabled: true
-          //   //   },
-          //   //   {
-          //   //     key: '周五',
-          //   //     disabled: false
-          //   //   },
-          //   //   {
-          //   //     key: '周六',
-          //   //     disabled: true
-          //   //   },
-          //   //   {
-          //   //     key: '周日',
-          //   //     disabled: true
-          //   //   }
-          //   // ]
-          // },
-          // {
-          //   title: '周日',
-          //   value: [6, 24]
-          // }
         ]
-      },
-      signUrl:
-        'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=234634259,4236876085&fm=27&gp=0.jpg',
-      bgUrl:
-        'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1986179278,1118313821&fm=27&gp=0.jpg'
+      }
     }
   },
   created() {
