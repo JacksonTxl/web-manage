a775f7b8085d08a83fb8b61f00f3e6eac205cc40
zhenglinxiong <zlx1433@gmail.com>
2019-04-09 14:26:01 +0800

    git regions

--- src/views/pages/brand/product/card/deposite-card/shop-sale-list/brand.vue
+++ src/views/pages/brand/product/card/deposite-card/shop-sale-list/brand.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    品牌私教课程库
+  <div class="page-shop-sale-list__brand">
+    <header>
+      <div class="page-shop-sale-list__brand--opreation">
+        <st-button type="primary">转入品牌私教课程库</st-button>
+        <div>
+          <a-select></a-select><a-select></a-select>
+        </div>
+      </div>
+      <div class="page-shop-sale-list__brand--count mg-t16">
+        <span>i</span><span>已选</span>
+      </div>
+    </header>
+    <main class="page-shop-sale-list__brand--table mg-t8">
+      <st-table></st-table>
+    </main>
   </div>
 </template>
 
