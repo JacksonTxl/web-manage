093521961dfa9d399bda00b88a35b4979a880b51
FlynnLee <593029372@qq.com>
2019-04-10 18:51:16 +0800

    update components register

--- src/views/components/register.ts
+++ src/views/components/register.ts
@@ -4,7 +4,6 @@ import { parse } from '@/utils/webpack-key-path'
 const ComponentsContext = require.context('@/views/components', true, /\.vue$/)
 const ComponentsKeys = ComponentsContext.keys()
 
-const routes: any = []
 ComponentsKeys.forEach(keyPath => {
   const file = ComponentsContext(keyPath)
   const component = file.default || file
