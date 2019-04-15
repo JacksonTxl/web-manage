47f4d1982f042e4925ea61fd0cd4a68a00ad6fae
FlynnLee <593029372@qq.com>
2019-04-15 19:28:17 +0800

    add editor example

--- src/views/components/editor/editor#.vue
+++ src/views/components/editor/editor#.vue
@@ -4,6 +4,72 @@
 
 <script>
 import { AppConfig } from '@/constants/config'
+
+const editorEvents = [
+  'activate',
+  'addUndo',
+  'beforeAddUndo',
+  'beforeExecCommand',
+  'beforeGetContent',
+  'beforeRenderUI',
+  'beforeSetContent',
+  'beforePaste',
+  'blur',
+  'change',
+  'clearUndos',
+  'click',
+  'contextMenu',
+  'copy',
+  'cut',
+  'dblclick',
+  'deactivate',
+  'dirty',
+  'drag',
+  'dragDrop',
+  'dragEnd',
+  'dragGesture',
+  'dragOver',
+  'drop',
+  'execCommand',
+  'focus',
+  'focusIn',
+  'focusOut',
+  'getContent',
+  'hide',
+  'init',
+  'keyDown',
+  'keyPress',
+  'keyUp',
+  'loadContent',
+  'mouseDown',
+  'mouseEnter',
+  'mouseLeave',
+  'mouseMove',
+  'mouseOut',
+  'mouseOver',
+  'mouseUp',
+  'nodeChange',
+  'objectResizeStart',
+  'objectResized',
+  'objectSelected',
+  'paste',
+  'postProcess',
+  'postRender',
+  'preProcess',
+  'progressState',
+  'redo',
+  'remove',
+  'reset',
+  'saveContent',
+  'selectionChange',
+  'setAttrib',
+  'setContent',
+  'show',
+  'submit',
+  'undo',
+  'visualAid'
+]
+
 export default {
   name: 'StEditor',
   props: {
@@ -27,9 +93,13 @@ export default {
       return src.indexOf('.js') > -1
     },
     loadJs(src) {
+      const s = document.createElement('script')
+      const url = this.appConfig.BASE_URL + src
+      if (document.querySelector(`script[src='${url}']`)) {
+        return Promise.resolve()
+      }
       return new Promise((resolve, reject) => {
-        const s = document.createElement('script')
-        s.src = this.appConfig.BASE_URL + src
+        s.src = url
         document.head.appendChild(s)
         s.onload = () => {
           resolve()
@@ -50,21 +120,30 @@ export default {
     }
   },
   mounted() {
-    this.dyamicLoad([
-      'tinymce/5.0.3/tinymce.min.js',
-      'tinymce/5.0.3/themes/silver/theme.min.js'
-    ]).then(() => {
-      window.tinyMCE
-        .init({
+    this.dyamicLoad(['tinymce/5.0.3/tinymce.min.js'])
+      .then(() => this.dyamicLoad(['tinymce/5.0.3/themes/silver/theme.min.js']))
+      .then(() => {
+        const ctx = this
+        window.tinyMCE.init({
           selector: '.' + this.editorId,
           language: 'zh_CN',
           menubar: false,
           language_url: this.appConfig.BASE_URL + 'tinymce/5.0.3/zh_CN.js',
           setup(editor) {
-            console.log(editor)
+            editor.on('change keyup undo redo', value => {
+              const currentContent = editor.getContent()
+              ctx.$emit('input', currentContent)
+            })
+            editorEvents.forEach(key => {
+              if (ctx.$listeners[key]) {
+                editor.on(key, e => {
+                  ctx.$emit(key, e, editor)
+                })
+              }
+            })
           }
         })
-    })
+      })
   }
 }
 </script>
