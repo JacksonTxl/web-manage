882db9e30643ca34718f8332f16d28dba2436799
jianghao <jianghao@xinchanedu.com>
2019-04-16 14:00:19 +0800

    change error

--- src/views/pages/brand/staff/edit.service.ts
+++ src/views/pages/brand/staff/edit.service.ts
@@ -4,7 +4,7 @@ import { ServiceRoute } from 'vue-service-app'
 export class EditService extends SetService {
   beforeRouteEnter(to: ServiceRoute, from: ServiceRoute, next: any) {
     console.log('this', this)
-    this.getCountryCodes({}).subscribe(res => {
+    this.getCountryCodes().subscribe(res => {
       console.log('mobel', res)
     })
     this.getStaffBrandReview('1').subscribe(res => {
