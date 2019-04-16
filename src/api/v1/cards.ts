bca43b0864023cbcfccb8774c0dd68c94d3ba72d
kael <apple@appledeMacBook-Pro.local>
2019-04-17 00:24:19 +0800

    card/info

--- src/api/v1/cards.ts
+++ src/api/v1/cards.ts
@@ -7,4 +7,19 @@ export class CardsApi extends Api {
   getCardsBgList() {
     return this.http.get('/v1/cards/bg_list', { mock: {} })
   }
+  /**
+   * 会员卡新增
+   */
+  addCards(params:any) {
+    return this.http.post('/v1/cards', { params })
+  }
+  /**
+   *
+   */
+  getCardInfo(id:string) {
+    return this.http.get(`/v1/cards/${id}`)
+  }
+}
+export interface CardsInput {
+  [propName: string]: any;
 }
