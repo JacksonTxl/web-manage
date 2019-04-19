2aa35c1a5ae57b5fbca4d447ae2677096cd6004e
FlynnLee <593029372@qq.com>
2019-04-19 10:42:21 +0800

    update example

--- src/views/pages/test/llf/mine.service.ts
+++ src/views/pages/test/llf/mine.service.ts
@@ -2,11 +2,12 @@ import { RouteGuard, ServiceRoute } from 'vue-service-app'
 import { State, Computed } from 'rx-state/src'
 import { Store } from '@/services/store'
 import { pluck } from 'rxjs/operators'
+import { RouteService } from '@/services/route.service'
 
 export class MineService extends Store<any> implements RouteGuard {
   state$: State<any>
   list$: Computed<any[]>
-  constructor() {
+  constructor(private routeService: RouteService) {
     super()
     this.state$ = new State({
       list: []
@@ -20,6 +21,8 @@ export class MineService extends Store<any> implements RouteGuard {
     next()
   }
   beforeRouteUpdate(to: ServiceRoute, from: ServiceRoute, next: any) {
+    // 格式化后的query快照对象 可使用此对象进行请求
+    console.log(this.routeService.query$.snapshot())
     this.state$.commit(state => {
       state.list = [1, 2, 3]
     })
