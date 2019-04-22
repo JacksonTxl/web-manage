e38923fb32125169fc0692614f37a54be575b7a2
FlynnLee <593029372@qq.com>
2019-04-22 23:15:31 +0800

    update sg

--- src/views/pages/styleguide/component#/image-upload.vue
+++ src/views/pages/styleguide/component#/image-upload.vue
@@ -9,22 +9,27 @@
     </p>
     <p class="mg-t8">默认样式 单图文件大小2m</p>
     <st-image-upload :list="list1" :sizeLimit="2"></st-image-upload>
+    {{list1}}
     <p class="mg-t8">自定义文字</p>
     <st-image-upload :list="list2" placeholder="请上传图片"></st-image-upload>
+    {{list2}}
     <p class="mg-t8">使用裁剪 可传入cropper模态窗选项和cropperjs选项参数</p>
     <st-image-upload
       :list="list3"
       placeholder="带裁剪的上传"
       :cropperModal="{title:'标题',cropper:{aspectRatio:1/1}}"
     ></st-image-upload>
+    {{list3}}
     <p class="mg-t8">自定义上传区样式</p>
     <st-image-upload :list="list4">
       <st-icon type="wechat"></st-icon>请上传拉
     </st-image-upload>
+    {{list4}}
     <p class="mg-t8">在已上传图片容器添加元素</p>
     <st-image-upload :list="list5">
       <span slot="item-extra" slot-scope="{item,index}">这是extra内容 {{item}} {{index}}</span>
     </st-image-upload>
+    {{list5}}
     <p class="mg-t8">自定义操作区内容</p>
     <st-image-upload :list="list6">
       <template slot="actions">
@@ -32,6 +37,7 @@
         <span class="action">招聘</span>
       </template>
     </st-image-upload>
+    {{list6}}
   </section>
 </template>
 
--- src/views/pages/styleguide/component.vue
+++ src/views/pages/styleguide/component.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <h2>Component 组件类</h2>
+    <p>组件具体的使用方式还是需要查看@/views/components/*.vue 对应文件查看最新的api，这里只是列举常用选项</p>
+
     <title-demo id="title"></title-demo>
     <button-demo id="button"></button-demo>
     <switch-demo id="switch"></switch-demo>
@@ -11,15 +13,15 @@
     <more-dropdown-demo id="more-dropdown"></more-dropdown-demo>
     <help-popover-demo id="help-popover"></help-popover-demo>
     <tag-demo id="tag"></tag-demo>
-    <region-cascader-demo id='region-cascader'></region-cascader-demo>
+    <region-cascader-demo id="region-cascader"></region-cascader-demo>
     <container-demo id="container"></container-demo>
     <panel-demo id="panel"></panel-demo>
-    <image-upload-demo id='image-upload'></image-upload-demo>
+    <image-upload-demo id="image-upload"></image-upload-demo>
     <info-demo id="info"></info-demo>
-    <form-demo id='form'></form-demo>
+    <form-demo id="form"></form-demo>
     <table-demo id="table"></table-demo>
     <form-table-demo id="form-table"></form-table-demo>
-    <shop-hour-picker-demo id='shop-hour-picker'></shop-hour-picker-demo>
+    <shop-hour-picker-demo id="shop-hour-picker"></shop-hour-picker-demo>
   </div>
 </template>
 
