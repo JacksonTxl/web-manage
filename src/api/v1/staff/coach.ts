5481b94770fafcacc0a8dd9fa93ae2c5865bf537
tenglei <tenglei@rrr.me>
2019-04-26 14:43:49 +0800

    commit

--- src/api/v1/staff/coach.ts
+++ src/api/v1/staff/coach.ts
@@ -3,7 +3,14 @@ export class CoachApi extends Api {
   getCoachSelect(params: GetSelectCoachInput) {
     return this.http.post('/v1/staff/coach/select', { params })
   }
+  /**
+   *
+   */
+  getCoachBasic(params: GetCoachBasicInput) {
+    return this.http.post('/v1/staff/coach/basic', { params })
+  }
 }
+
 export interface GetSelectCoachInput {
   /**
    * 搜索关键字
@@ -17,4 +24,11 @@ export interface GetSelectCoachInput {
    * 门店id集合
    */
   shop_ids: number[]
+}
+
+export interface GetCoachBasicInput {
+  /**
+   * 教练id集合
+   */
+  coach_ids: number[]
 }
