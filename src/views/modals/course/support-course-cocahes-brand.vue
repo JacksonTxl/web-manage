e589238dec0abad46945d353ccc53b5b06a0454b
zhenglinxiong <zlx1433@gmail.com>
2019-05-05 17:54:28 +0800

    update code

--- src/views/modals/course/support-course-cocahes-brand.vue
+++ src/views/modals/course/support-course-cocahes-brand.vue
@@ -4,7 +4,10 @@
     :title="course.course_name + ' 查看授课教练'"
     v-model='show'>
     <div class="modal-support-course-shops__search">
-
+      <a-select showSearch :defaultValue="defaultValue"  :filterOption="filterOption" optionFilterProp="children" class="mg-r8" style="width: 160px" @change="onChange">
+        <a-select-option v-for="category in categoryList" :key="category.id" :value="category.id">{{category.setting_name}}</a-select-option>
+      </a-select>
+      <st-input-search placeholder="通过教练项目、昵称、手机号查询"></st-input-search>
     </div>
     <st-container>
     <st-table
@@ -30,6 +33,7 @@ export default {
     return {
       columnsCoaches,
       show: false,
+      courseStatus: [{ label: '所有状态', value: -1 }, { label: '有效', value: 1 }, { label: '无效', value: 0 }],
       supportCoachList: []
     }
   },
