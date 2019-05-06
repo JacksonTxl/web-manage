1fca636ae8c076d04deab057cbab6a25accb72d6
崔庆 <cq_weifeng@163.com>
2019-05-06 11:23:05 +0800

    style

--- src/views/pages/shop/member/info.vue
+++ src/views/pages/shop/member/info.vue
@@ -3,7 +3,7 @@
     <st-panel class="shop-member-info-title">
       <a-row :gutter="24">
         <a-col :span="12">
-          <div class="shop-member-info-title-pannel">
+          <div class="shop-member-info-title-pannel-left">
             <div class="shop-member-info-title-pannel__img">
               <div class="img-box">
                 <img
@@ -21,7 +21,7 @@
             </div>
 
             <div class="shop-member-info-title-pannel__info">
-              <st-t2>积分</st-t2>
+              <st-t2>孙乐乐</st-t2>
               <div class="shop-member-info-title-pannel__info__phone">
                 <div>
                   手机号:
@@ -32,6 +32,62 @@
                   首次成为会员时间:
                   <span>1888888888</span>
                 </div>
+              </div>
+              <div class="label-list">
+                <span class="lable-item">
+                  胖子
+                  <a-icon type="close"/>
+                </span>
+                <span class="lable-item">
+                  快乐的小逗比一个
+                  <a-icon type="close"/>
+                </span>
+                <span class="lable-item">
+                  胖子
+                  <a-icon type="close"/>
+                </span>
+              </div>
+              <div class="add-lable">
+                <span class="add-lable-item">
+                  <a-icon type="plus"/>标签
+                </span>
+              </div>
+            </div>
+          </div>
+        </a-col>
+        <a-col :span="12">
+          <div class="shop-member-info-title-pannel-right">
+            <div class="pannel-right__operation">
+              <a-button type="primary" class="pannel-right__operation__margin">编辑资料</a-button>
+              <a-button class="pannel-right__operation__margin">绑定实体卡</a-button>
+              <a-select
+                class="pannel-right__operation__margin"
+                defaultValue="更多操作"
+                style="width: 120px"
+              >
+                <a-select-option value="jack">Jack</a-select-option>
+                <a-select-option value="lucy">Lucy</a-select-option>
+                <a-select-option value="Yiminghe">yiminghe</a-select-option>
+              </a-select>
+            </div>
+            <div class="pannel-right__operation">
+              <div class="pannel-right__num__box">
+                <span class="pannel-right__num">3</span>
+                <span class="pannel-right__num__margin">/</span>
+                <span>张</span>
+                <p>有效会员卡</p>
+              </div>
+              <div class="pannel-right__num__box">
+                <span class="pannel-right__num">3</span>
+                <span class="pannel-right__num__margin">/</span>
+                <span>张</span>
+                <p>有效会员卡</p>
+              </div>
+              <div class="pannel-right__num__box">
+                <span class="pannel-right__num">3</span>
+                <span class="pannel-right__num__margin">/</span>
+                <span>张</span>
+                <p>有效会员卡</p>
               </div>
             </div>
           </div>
