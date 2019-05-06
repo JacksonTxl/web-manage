4547758c17125f4e956c14d5bbee106cdd5d7823
tenglei <tenglei@rrr.me>
2019-05-06 15:51:19 +0800

    help

---
+++ src/views/components/help/help.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    help
+  </div>
+</template>
+<script>
+import { HelpService } from './help.service'
+export default {
+  name: 'StHelp',
+  serviceInject() {
+    return {
+      helpService: HelpService
+    }
+  }
+}
+</script>
