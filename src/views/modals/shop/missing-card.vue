45e2de3e11b8b2272c3697ea41c2a0aa0a4713c2
崔庆 <cq_weifeng@163.com>
2019-05-07 21:03:15 +0800

    missing-card

--- src/views/modals/shop/missing-card.vue
+++ src/views/modals/shop/missing-card.vue
@@ -1,6 +1,43 @@
 <template>
   <st-modal title="遗失补卡" @ok="save" v-model="show" size="small">
-    <section>content</section>
+    <section>
+      <section>
+        <st-form :form="form" @submit="save" labelWidth="55px">
+          <st-info>
+            <st-info-item label="姓名">孙乐乐</st-info-item>
+            <st-info-item label="手机号">12345678901</st-info-item>
+          </st-info>
+          <st-form-item label="实体卡号">
+            <a-input placeholder="输入实体卡号"/>
+          </st-form-item>
+          <div
+            style="background:rgba(247,249,252,1);padding: 20px 20px 1px 20px;margin-bottom:20px"
+          >
+            <st-form :form="form" @submit="save" labelWidth="125px">
+              <st-info>
+                <st-info-item label="当前绑定的实体卡号">213456758987654</st-info-item>
+                <st-info-item label="当前物理ID">12345678901</st-info-item>
+              </st-info>
+            </st-form>
+          </div>
+          <st-form-item label="物理ID">
+            <a-input placeholder="请将实体卡置于读卡器上"/>
+          </st-form-item>
+          <st-form-item label="手续费">
+            <st-input-number v-model="inputnumber2" :float="true" placeholder="请将实体卡置于读卡器上">
+              <template slot="addonAfter">元</template>
+            </st-input-number>
+          </st-form-item>
+
+          <st-form-item label="支付方式">
+            <a-input placeholder="输入收款的支付方式"/>
+          </st-form-item>
+          <st-form-item label="收款人员">
+            <a-input placeholder="输入收款的工作人员"/>
+          </st-form-item>
+        </st-form>
+      </section>
+    </section>
     <section>
       <!-- <footer class="footer">
         <a-button class="cancel" @click="show=false">取消</a-button>
@@ -15,7 +52,9 @@ export default {
   props: {},
   data() {
     return {
-      show: false
+      show: false,
+      form: {},
+      inputnumber2: ''
     }
   },
   created() {},
