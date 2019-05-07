d22ddea23aa5df38c1058e7465957c2499f09fe3
崔庆 <cq_weifeng@163.com>
2019-05-07 14:25:15 +0800

    aa

---
+++ src/views/modals/card/table-stop.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, ServiceRoute } from 'vue-service-app'
+import { State, Computed, Effect, Action } from 'rx-state'
+import { pluck } from 'rxjs/operators'
+import { Store } from '@/services/store'
+import { CardsApi } from '@/api/v1/cards'
+
+interface CardsTableModelState {
+  cardsTableModel: any
+}
+@Injectable()
+export class TableStopService extends Store<CardsTableModelState> {
+  state$: State<CardsTableModelState>
+  cardsTableModel$: Computed<string>
+  constructor(private cardsApi: CardsApi) {
+    super()
+    this.state$ = new State({
+      cardsTableModel: {}
+    })
+    this.cardsTableModel$ = new Computed(
+      this.state$.pipe(pluck('cardsTableModel'))
+    )
+  }
+  SET_CARDS_Table_INFO(cardsTableModel: CardsTableModelState) {
+    this.state$.commit(state => {
+      state.cardsTableModel = cardsTableModel
+    })
+  }
+  getListInfo(paramsObj: any) {
+    return this.cardsApi.getCardsUseShop(paramsObj)
+  }
+  getCardsDepositSaleShop(paramsObj: any) {
+    return this.cardsApi.getCardsDepositSaleShop(paramsObj)
+  }
+}
