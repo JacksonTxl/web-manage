1228ed01d91c7d5c856c34ea6494f5c558f86a49
tenglei <tenglei@rrr.me>
2019-05-08 11:49:47 +0800

    私教课预约设置

--- src/api/v1/setting/course/personal/reserve.ts
+++ src/api/v1/setting/course/personal/reserve.ts
@@ -1,5 +1,5 @@
 import { Api } from '@/api/api'
-const url = '/v1/setting/course/personal_reserve'
+const url = '/v1/setting/course/personal/reserve'
 
 export class PersonReserveSettingApi extends Api {
   /**
@@ -11,7 +11,43 @@ export class PersonReserveSettingApi extends Api {
   /**
    * 私教课课程预约设置编辑
    */
-  update() {
-    return this.http.put(url)
+  update(params: UpdateInput) {
+    return this.http.put(url, { params })
   }
+}
+
+export interface UpdateInput {
+  /**
+   * 私教预约范围开始选项,1 1小时后, 2 2小时后….12 12小时后
+   */
+  reserve_start: number,
+  /**
+   * 私教可预约范围: 1 1周 2 2 周 3 3周
+   */
+  reserve_limit: number,
+  /**
+   * 私教取消预约时间限制: 1 1小时后, 2 2小时后….12 12小时后
+   */
+  cancel_reserve: number,
+  /**
+   * 私教预约提醒选项: 1 1小时 2 2 小时 3 4小时 4 6小时 5 12小时 6 24小时
+   */
+  reserve_remind: number,
+  /**
+   * 私教是否支持用户自主签到: 1是 0否
+   */
+  is_sign: number,
+  /**
+   * 私教课程签到时间限制: 1 0分钟 2 10分钟 3 20分钟 4 30分钟 5 40分钟
+   */
+  sign_time: number,
+  /**
+   * 是否私教课程系统自动签到: 1 是 0 否
+   */
+  is_auto_sign: number,
+  /**
+   * 私教课程系统自动签到时间: 1 1 天 2 2天 3 3天
+   */
+
+  auto_sign_limit: number
 }
--- src/views/modals/brand/setting-personal-reserve.vue
+++ src/views/modals/brand/setting-personal-reserve.vue
@@ -3,6 +3,8 @@
     title="私教课预约设置"
     class="modal-personal-order-setting"
     v-model="show"
+    @ok="save"
+    @cancel="cancel"
   >
     <div>
       <st-form :form="form" labelWidth="56px">
@@ -10,12 +12,12 @@
         <st-form-item label="预约范围">
           <div>
             <span>用户可预约</span>
-            <a-select class="mg-l8" style="width: 100px">
-              <a-select-option value="3">3小时后</a-select-option>
+            <a-select class="mg-l8" style="width: 100px" v-model="info.reserve_start">
+              <a-select-option :value="1">1小时后</a-select-option>
             </a-select>
             <span class="mg-l8" >到</span>
-            <a-select class="mg-l8" style="width: 66px">
-              <a-select-option value="3">1周</a-select-option>
+            <a-select class="mg-l8" style="width: 70px" v-model="info.reserve_range">
+              <a-select-option :value="1">1周</a-select-option>
             </a-select>
             <span class="mg-l8">以内的私教课程，员工代预约不受此限制</span>
           </div>
@@ -24,8 +26,8 @@
         <st-form-item label="取消预约">
           <div>
             <span>允许用户在私教课程开始前</span>
-            <a-select class="mg-l8" style="width: 84px">
-              <a-select-option value="1">1小时</a-select-option>
+            <a-select class="mg-l8" style="width: 84px" v-model="info.cancel_reserve">
+              <a-select-option :value="1">1小时</a-select-option>
             </a-select>
             <span class="mg-l8" >取消预约，员工代取消不受此限制</span>
           </div>
@@ -35,7 +37,7 @@
         <st-form-item label="预约提醒">
           <div>
             <span>在课程开始前</span>
-            <a-select class="mg-l8" style="width: 84px">
+            <a-select class="mg-l8" style="width: 84px" v-model="info.reserve_remind">
               <a-select-option value="1">1小时</a-select-option>
             </a-select>
             <span class="mg-l8" >提醒用户上课</span>
@@ -43,13 +45,13 @@
         </st-form-item>
         <!-- 签到方式 -->
         <st-form-item label="签到方式">
-          <a-checkbox :checked="true">支持用户自主签到</a-checkbox>
+          <a-checkbox :checked="!!info.is_sign" @change="onCheckboxChange('is_sign')">支持用户自主签到</a-checkbox>
         </st-form-item>
         <!-- 签到时间 -->
         <st-form-item label="签到时间">
           <div>
             <span>开课</span>
-            <a-select class="mg-l8" style="width: 120px">
+            <a-select class="mg-l8" style="width: 120px" v-model="info.sign_time">
               <a-select-option value="1">即刻</a-select-option>
             </a-select>
             <span class="mg-l8" >允许签到</span>
@@ -58,8 +60,8 @@
         <!-- 自动签到 -->
         <st-form-item label="自动签到">
           <div>
-            <span>支持系统自动签到，在</span>
-            <a-select class="mg-l8" style="width: 84px">
+            <a-checkbox :checked="!!info.is_auto_sign" @change="onCheckboxChange('is_auto_sign')">支持系统自动签到，在</a-checkbox>
+            <a-select class="mg-l8" style="width: 84px" v-model="info.auto_sign_limit">
               <a-select-option value="1">1天</a-select-option>
             </a-select>
             <span class="mg-l8" >后系统自动将课程进行签到</span>
@@ -71,11 +73,42 @@
   </st-modal>
 </template>
 <script>
+import { MessageService } from '@/services/message.service'
+import { PersonalReserveSettingService } from './setting-personal-reserve.service'
 export default {
+  serviceInject() {
+    return {
+      messageService: MessageService,
+      settingService: PersonalReserveSettingService
+    }
+  },
   data() {
     return {
       show: false,
-      form: this.$form.createForm(this)
+      form: this.$form.createForm(this),
+      info: {}
+    }
+  },
+  created() {
+    this.settingService.getInfo().subscribe(res => {
+      this.info = res.info
+    })
+  },
+  methods: {
+    onCheckboxChange(index) {
+      this.info[index] = +!this.info[index]
+    },
+    cancel() {
+      this.show = false
+    },
+    save() {
+      this.settingService.update({ ...this.info }).subscribe(
+        () => {
+          this.messageService.success({
+            content: '提交成功'
+          })
+        }
+      )
     }
   }
 }
