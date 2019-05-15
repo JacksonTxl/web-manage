3030d41836f4b27067bdba668357553b8979f66a
tenglei <tenglei@rrr.me>
2019-05-15 11:28:08 +0800

    page route nav

--- src/views/pages/brand/setting/general.vue
+++ src/views/pages/brand/setting/general.vue
@@ -1,7 +1,7 @@
 <template>
   <div :class="b()">
     <div :class="b('navs')">
-      <general-nav></general-nav>
+      <page-route-nav title="通用设置" v-model="navs"></page-route-nav>
     </div>
     <div :class="b('content')">
       <router-view></router-view>
@@ -9,13 +9,43 @@
   </div>
 </template>
 <script>
-import GeneralNav from './nav#/general'
+import PageRouteNav from '@/views/components/page-route-nav#/nav'
 export default {
   bem: {
     b: 'page-setting'
   },
   components: {
-    GeneralNav
+    PageRouteNav
+  },
+  data() {
+    return {
+      navs: [{
+        label: '品牌设置',
+        route: {
+          name: 'brand-setting-general-brand'
+        }
+      }, {
+        label: '课程定价及预约设置',
+        route: {
+          name: 'brand-setting-general-course-personal'
+        }
+      }, {
+        label: '支付设置',
+        route: {
+          name: 'brand-setting-general-pay-type'
+        }
+      }, {
+        label: '教练等级设置',
+        route: {
+          name: 'brand-setting-general-coach-level'
+        }
+      }, {
+        label: '用户等级设置',
+        route: {
+          name: 'brand-setting-general-user-level'
+        }
+      }]
+    }
   }
 }
 </script>
