f9fce216c4221a26977096457c014356b85dd5ad
崔庆 <cq_weifeng@163.com>
2019-05-16 19:57:17 +0800

    sold

--- src/views/pages/shop/member/crowd/add.vue
+++ src/views/pages/shop/member/crowd/add.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-    新建人群
+  <div class="shop-member-crowd-add">
+    <div class="shop-member-crowd-add__box">
+      <div class="shop-member-crowd-add__left">
+        <st-t2>人群定义维度</st-t2>
+        <div style="padding-top:24px;coler:#9BACB9">单个人群最多可添加5个条件</div>
+        <st-t4>基础资料</st-t4>
+      </div>
+      <div class="shop-member-crowd-add__right">right</div>
+    </div>
   </div>
 </template>
