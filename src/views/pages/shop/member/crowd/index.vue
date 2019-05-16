ae59125cc96dde03a58b9755e0610530dc138ada
崔庆 <cq_weifeng@163.com>
2019-05-16 19:14:42 +0800

    cromber

--- src/views/pages/shop/member/crowd/index.vue
+++ src/views/pages/shop/member/crowd/index.vue
@@ -1,69 +1,118 @@
 <template>
-  <div>
-    <div class="shop-member-crowd-index">
-      <div class="shop-member-crowd-index-title">
-        <div style=" padding: 32px 28px 0 32px;">
-          <div class="shop-member-crowd-index__item">
-            <img
-              src="http://n.sinaimg.cn/sinacn14/39/w480h359/20181013/e040-hmivixm5109047.jpg"
-              alt="ccccc"
-              class="shop-member-crowd-index__img"
-            >
-            <div class="shop-member-crowd-index__info">
-              <st-t4 class="shop-member-crowd-index__info__title">新注册用户</st-t4>
-              <div>近7天内，新注册的潜在用户</div>
-            </div>
-          </div>
-          <div class="shop-member-crowd-index__num">
-            <div class="shop-member-crowd-index__num-item">
-              <div style="margin-bottom: 16px;">10</div>
-              <div>人群总数</div>
-            </div>
-            <div class="shop-member-crowd-index__num-item">
-              <div style="margin-bottom: 16px;">10</div>
-              <div>人群总数</div>
-            </div>
-            <div class="shop-member-crowd-index__num-item">
-              <div style="margin-bottom: 16px;">10</div>
-              <div>人群总数</div>
-            </div>
-          </div>
-        </div>
-        <div style="height: 40px;background:rgba(247,249,252,1);">
-          <span
-            style="height:24px;line-height: 24px;display: inline-block;width:50%;margin:8px 0;text-align: center;border-right:1px solid rgba(230,233,239,1);"
-          >人群分析</span>
-          <span
-            style="height:24px;line-height: 24px;display: inline-block;width:50%;margin:8px 0;text-align: center;"
-          >
-            <a-dropdown>
-              <span class="ant-dropdown-link" href="#">
-                定向运营
-                <a-icon type="down"/>
-              </span>
-              <a-menu slot="overlay">
-                <a-menu-item>
-                  <a href="javascript:;">1st menu item</a>
-                </a-menu-item>
-                <a-menu-item>
-                  <a href="javascript:;">2nd menu item</a>
-                </a-menu-item>
-                <a-menu-item>
-                  <a href="javascript:;">3rd menu item</a>
-                </a-menu-item>
-              </a-menu>
-            </a-dropdown>
-          </span>
-        </div>
-      </div>
-      <div class="shop-member-crowd-index-title" style="margin:0 12px;">1</div>
-      <div class="shop-member-crowd-index-title">1</div>
-    </div>
+  <div class="shop-member-crowd-index">
+    <index v-model="indexData"></index>
     <st-panel>
       <div slot="title">
-        <st-button type="primary">新建人群</st-button>
+        <router-link tag="a" :to=" { name: 'shop-member-crowd-add'}">
+          <st-button type="primary">新建人群</st-button>
+        </router-link>
         <span class="shop-member-crowd-index-new__crowb_num" style>新建人群数量最多10个</span>
-      </div>content
+      </div>
+      <st-table
+        rowKey="id"
+        :dataSource="table.list"
+        :columns="table.columns1"
+        @change="onChange"
+        :pagination="pagination"
+      >
+        <!-- <div slot="shop_name1" slot-scope="text, record"> -->
+        <div slot="shop_name1">
+          <a href="#">导出</a>
+          <a-divider type="vertical"></a-divider>
+          <a href="#">编辑</a>
+          <a-divider type="vertical"></a-divider>
+          <a href="#">删除</a>
+        </div>
+        <div
+          slot="city_name"
+          slot-scope="text"
+          style="width:300px;overflow:hidden;text-overflow:ellipsis;white-space:nowrap;"
+        >
+          <a-tooltip placement="topLeft" overlayClassName="shop-member-crowd-index-tooltip">
+            <template slot="title">
+              <span>{{text}}</span>
+            </template>
+            {{text}}
+          </a-tooltip>
+        </div>
+      </st-table>
     </st-panel>
   </div>
 </template>
+<script>
+import index from './private-components#/index'
+export default {
+  data() {
+    return {
+      indexData: { aa: 1 },
+      pagination: {
+        pageSizeOptions: ['10', '20', '30', '40', '50'],
+        current: 1,
+        pageSize: 10,
+        total: 50
+      },
+      table: {
+        columns1: [
+          {
+            title: '人群名称',
+            dataIndex: 'province_name',
+            scopedSlots: { customRender: 'province_name' }
+          },
+          {
+            title: '人群定义',
+            width: '30%',
+            dataIndex: 'city_name',
+            scopedSlots: { customRender: 'city_name' }
+          },
+          {
+            title: '人群总数',
+            dataIndex: 'district_name'
+          },
+          {
+            title: '更新时间',
+            dataIndex: 'shop_name'
+          },
+          {
+            title: '操作',
+            width: '160px',
+            dataIndex: 'shop_name1',
+            scopedSlots: { customRender: 'shop_name1' }
+          }
+        ],
+        list: [
+          {
+            id: 1,
+            province_name: 1,
+            city_name:
+              '啊实打实大苏打实打实大苏打实打实大苏打士大夫十分三国杀是否啊大大啊实打实大苏打实打实大苏打实打实大苏打士大夫十分三国杀是否啊大大',
+            district_name: 1,
+            shop_name: 1,
+            shop_name1: 1
+          }
+        ]
+      }
+    }
+  },
+  components: {
+    index
+  },
+  methods: {
+    onChange(pagination, filters, sorter) {
+      console.log('params', pagination, filters, sorter, data)
+    },
+    groupSMS(value) {
+      console.log(value)
+    },
+    addTreeNode(value) {
+      console.log(value)
+    },
+    editTreeNode(value) {
+      console.log(value)
+    },
+    deleteTreeNode(value) {
+      console.log(value)
+    }
+  },
+  mounted() {}
+}
+</script>
