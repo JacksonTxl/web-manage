5c1574dea7b789157ead35d53fedcc5485810520
tenglei <tenglei@rrr.me>
2019-05-16 11:12:53 +0800

    user level

--- src/api/v1/setting/member.ts
+++ src/api/v1/setting/member.ts
@@ -1,10 +1,46 @@
 import { Api } from '@/api/api'
-const url = '/v1/setting/member'
 export class SettingMemberApi extends Api {
   /**
    * 获取用户等级设置列表
    */
   getList() {
-    return this.http.get(url)
+    return this.http.get('/v1/setting/member')
   }
+  /**
+   * 用户等级设置等级条件回显
+   */
+  getCondition() {
+    return this.http.get('/v1/setting/member/condition/2')
+  }
+  /**
+   * 用户等级设置等级权益回显
+   */
+  getRights() {
+    return this.http.get('/v1/setting/member/rights/2')
+  }
+  /**
+   * 用户等级设置等级条件编辑
+   */
+  updateCondation(params: UpdateConditionInput) {
+    return this.http.put('/v1/setting/member/condition/2')
+  }
+  /**
+   * 用户等级设置等级权益编辑
+   */
+  updateRights(params: UpdateRightsInput) {
+    return this.http.put('/v1/setting/member/rights/2')
+  }
+}
+
+export interface UpdateConditionInput {
+  /**
+   * 用户等级条件id
+   */
+  condition: number[]
+}
+export interface UpdateRightsInput {
+  /**
+   * 用户等级权益id
+   */
+  rights: number[]
 }
