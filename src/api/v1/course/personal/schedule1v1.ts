e91730861af68fd18e393f1721e3cfa759a7a112
jianghao <jianghao@xinchanedu.com>
2019-05-24 10:50:27 +0800

    add schedule personal

--- src/api/v1/course/personal/schedule1v1.ts
+++ src/api/v1/course/personal/schedule1v1.ts
@@ -2,14 +2,7 @@ import { PostScheduleTeamCopyInput } from './../team/schedule'
 
 import { Api } from '@/api/api'
 import { GetScheduleSiteListInput } from '../team/schedule'
-/**
- *
- *
- * @export
- * @class ScheduleApi
- * @extends {Api}
- * 团体课排期接口
- */
+
 export class ScheduleApi1v1 extends Api {
   /**
    * 私教排期列表
