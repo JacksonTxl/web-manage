eca22a9d95f17df1216e2b0969a020feb49f538d
tenglei <tenglei@rrr.me>
2019-05-24 13:11:55 +0800

    私教小团课

--- src/views/pages/shop/product/course/schedule/personal-team.service#/common.service.ts
+++ src/views/pages/shop/product/course/schedule/personal-team.service#/common.service.ts
@@ -1,42 +1,37 @@
-import { TeamScheduleCommonApi, UnUsedSeatQuery, ConsumeQuery } from '@/api/v1/schedule/team/common'
 import { Injectable } from 'vue-service-app'
 import { State, Computed } from 'rx-state/src'
 import { tap, pluck } from 'rxjs/operators'
 import { GetMemberInput } from '@/api/v1/course/team/schedule'
+import {
+  PersonalTeamScheduleCommonApi,
+  ConsumeQuery
+} from '@/api/v1/schedule/personal-team/common'
 
 export interface SetState {
   courseOptions: any[],
   coachOptions: any[],
   memberOptions: any[],
-  courtOptions: any[],
-  unUsedSeatOptions: any[],
   consumeOptions: any[]
 }
 @Injectable()
-export class TeamScheduleCommonService {
+export class PersonalTeamScheduleCommonService {
   state$: State<SetState>
   courseOptions$: Computed<any[]>
   coachOptions$: Computed<any[]>
   memberOptions$: Computed<any[]>
-  courtOptions$: Computed<any[]>
-  unUsedSeatOptions$: Computed<any[]>
   consumeOptions$: Computed<any[]>
 
-  constructor(private commonApi: TeamScheduleCommonApi) {
+  constructor(private commonApi: PersonalTeamScheduleCommonApi) {
     this.state$ = new State({
       courseOptions: [],
       coachOptions: [],
-      unUsedSeatOptions: [],
       memberOptions: [],
-      consumeOptions: [],
-      courtOptions: []
+      consumeOptions: []
     })
     this.consumeOptions$ = new Computed(this.state$.pipe(pluck('consumeOptions')))
-    this.unUsedSeatOptions$ = new Computed(this.state$.pipe(pluck('unUsedSeatOptions')))
     this.courseOptions$ = new Computed(this.state$.pipe(pluck('courseOptions')))
     this.coachOptions$ = new Computed(this.state$.pipe(pluck('coachOptions')))
     this.memberOptions$ = new Computed(this.state$.pipe(pluck('memberOptions')))
-    this.courtOptions$ = new Computed(this.state$.pipe(pluck('courtOptions')))
   }
   /**
    *
@@ -44,23 +39,13 @@ export class TeamScheduleCommonService {
    * 获取会员Options
    */
   getMemberList(query: GetMemberInput) {
-    return this.commonApi.getMemberList(query).pipe(tap(res => {
-      this.state$.commit(state => {
-        state.memberOptions = res.list
+    return this.commonApi.getMemberList(query).pipe(
+      tap(res => {
+        this.state$.commit(state => {
+          state.memberOptions = res.list
+        })
       })
-    }))
-  }
-  /**
-   *
-   * @param query
-   * 获取场地座位Options
-   */
-  getUnusedSeat(query: UnUsedSeatQuery) {
-    return this.commonApi.getUnusedSeat(query).pipe(tap(res => {
-      this.state$.commit(state => {
-        state.unUsedSeatOptions = res.list
-      })
-    }))
+    )
   }
   /**
    *
@@ -68,16 +53,18 @@ export class TeamScheduleCommonService {
    * 获取课程Options
    */
   getCourseList() {
-    return this.commonApi.getCourseList().pipe(tap(res => {
-      this.state$.commit(state => {
-        state.courseOptions = res.list
+    return this.commonApi.getCourseList().pipe(
+      tap(res => {
+        this.state$.commit(state => {
+          state.courseOptions = res.list
+        })
       })
-    }))
+    )
   }
   /**
    *
    * @param query
-   * 获取教练Options
+   * 获取会员Options
    */
   getCoachList() {
     return this.commonApi.getCoachList().pipe(tap(res => {
@@ -87,33 +74,9 @@ export class TeamScheduleCommonService {
     }))
   }
   /**
-  *
-  * @param query
-  * 获取场地Options
-  */
-  getCourtList() {
-    return this.commonApi.getCourtList().pipe(tap(res => {
-      this.state$.commit(state => {
-        state.courtOptions = res.list
-      })
-    }))
-  }
-  /**
    *
    * @param query
-   * 获取座位Options
-   */
-  getUnusedSeatList(query: UnUsedSeatQuery) {
-    return this.commonApi.getUnusedSeatList(query).pipe(tap(res => {
-      this.state$.commit(state => {
-        state.unUsedSeatOptions = res.list.map((item: any) => { return { id: item, name: item } })
-      })
-    }))
-  }
-  /**
-   *
-   * @param query
-   * 获取消费方式Options
+   * 获取会员Options
    */
   getConsumeList(query: ConsumeQuery) {
     return this.commonApi.getConsumeList(query).pipe(
