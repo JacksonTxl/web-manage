790fad5b2c4f1d925ada9b9d795b893cf51b0149
FlynnLee <lilingfei@styd.cn>
2019-06-07 23:25:54 +0800

    t1

--- src/main.ts
+++ src/main.ts
@@ -1,67 +1,67 @@
-import '@abraham/reflection'
-import { router } from './bootstrap'
-import Vue from 'vue'
-import Antd from 'ant-design-vue'
+// import '@abraham/reflection'
+// import { router } from './bootstrap'
+// import Vue from 'vue'
+// import Antd from 'ant-design-vue'
 
-import { modalRouter } from './modal-router/index'
-import App from './views/App.vue'
-import FullCalendar from 'vue-full-calendar'
-// @ts-ignore
-import VueBemHelper from '@/vendor/vue-bem-helper'
-// @ts-ignore
-import VueRxState from '@/vendor/vue-rx-state'
-import ICountUp from 'vue-countup-v2'
-// @ts-ignore
-import Viewer from 'v-viewer'
-// 默认样式加载
-import './style/index'
-import './views/components/register'
-import './filters/register'
+// import { modalRouter } from './modal-router/index'
+// import App from './views/App.vue'
+// import FullCalendar from 'vue-full-calendar'
+// // @ts-ignore
+// import VueBemHelper from '@/vendor/vue-bem-helper'
+// // @ts-ignore
+// import VueRxState from '@/vendor/vue-rx-state'
+// import ICountUp from 'vue-countup-v2'
+// // @ts-ignore
+// import Viewer from 'v-viewer'
+// // 默认样式加载
+// import './style/index'
+// import './views/components/register'
+// import './filters/register'
 
-// @ts-ignore
-import Scrollbar from '@/vendor/vue-scrollbar'
+// // @ts-ignore
+// import Scrollbar from '@/vendor/vue-scrollbar'
 
-Vue.use(Scrollbar)
-Vue.use(VueRxState)
-Vue.use(Antd)
-Vue.use(FullCalendar)
-Vue.use(Viewer)
-Vue.use(VueBemHelper)
+// Vue.use(Scrollbar)
+// Vue.use(VueRxState)
+// Vue.use(Antd)
+// Vue.use(FullCalendar)
+// Vue.use(Viewer)
+// Vue.use(VueBemHelper)
 
-Vue.component(ICountUp.name, ICountUp)
+// Vue.component(ICountUp.name, ICountUp)
 
-const app = new Vue({
-  el: '#app',
-  router,
-  // @ts-ignore
-  modalRouter,
-  render: h => h(App)
-})
+// const app = new Vue({
+//   el: '#app',
+//   router,
+//   // @ts-ignore
+//   modalRouter,
+//   render: h => h(App)
+// })
 
-const deperated = (oldName: string, serviceName: string) => {
-  return function() {
-    console.warn(
-      `this.$${oldName} is disabled,you should use serviceInject() to use ${serviceName} instead`
-    )
-  }
-}
+// const deperated = (oldName: string, serviceName: string) => {
+//   return function() {
+//     console.warn(
+//       `this.$${oldName} is disabled,you should use serviceInject() to use ${serviceName} instead`
+//     )
+//   }
+// }
 
-Object.defineProperty(Vue.prototype, '$notification', {
-  value: {
-    warn: deperated('notification.warn', 'notificationService'),
-    info: deperated('notification.info', 'notificationService'),
-    error: deperated('notification.error', 'notificationService'),
-    warning: deperated('notification.warning', 'notificationService'),
-    success: deperated('notification.success', 'notificationService')
-  }
-})
+// Object.defineProperty(Vue.prototype, '$notification', {
+//   value: {
+//     warn: deperated('notification.warn', 'notificationService'),
+//     info: deperated('notification.info', 'notificationService'),
+//     error: deperated('notification.error', 'notificationService'),
+//     warning: deperated('notification.warning', 'notificationService'),
+//     success: deperated('notification.success', 'notificationService')
+//   }
+// })
 
-Object.defineProperty(Vue.prototype, '$message', {
-  value: {
-    warn: deperated('message.warn', 'messageService'),
-    info: deperated('message.info', 'messageService'),
-    error: deperated('message.error', 'messageService'),
-    warning: deperated('message.warning', 'messageService'),
-    success: deperated('message.success', 'messageService')
-  }
-})
+// Object.defineProperty(Vue.prototype, '$message', {
+//   value: {
+//     warn: deperated('message.warn', 'messageService'),
+//     info: deperated('message.info', 'messageService'),
+//     error: deperated('message.error', 'messageService'),
+//     warning: deperated('message.warning', 'messageService'),
+//     success: deperated('message.success', 'messageService')
+//   }
+// })
