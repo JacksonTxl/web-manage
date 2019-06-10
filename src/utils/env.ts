bd70fd0726619226071c31e18e4821c690d6b4b8
tenglei <tenglei@rrr.me>
2019-06-10 12:01:40 +0800

    env

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -2,13 +2,15 @@
  * 获取 saas 环境 dev/prod
  */
 export function getAppEnv() {
-  const { origin } = location
-  const envRegExps = {
-    dev: /localhost|dev/
-  }
-  if (envRegExps.dev.test(origin)) {
-    return 'dev'
-  } else {
-    return ''
-  }
+  // const { origin } = location
+  // const envRegExps = {
+  //   dev: /localhost|dev/,
+  //   test: /test/
+  // }
+  return 'dev'
+  // if (envRegExps.dev.test(origin)) {
+  //   return 'dev'
+  // } else {
+  //   return ''
+  // }
 }
