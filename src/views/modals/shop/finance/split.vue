9f3c4e00b5724b529f7462ee28af0b72d25a3d1c
JacksonTxl <172004133@qq.com>
2019-06-20 17:35:59 +0800

    add finance order list

---
+++ src/views/modals/shop/finance/split.vue
@@ -0,0 +1,208 @@
+<template>
+  <st-modal
+  title="业务拆分"
+  size="small"
+  width="876"
+  v-model="show"
+  wrapClassName="modal-shop-finance-split">
+    <div :class="basic('content')">
+      <a-row :class="basic('info')">
+        <a-col :span="12" class="mgb-36">
+          <st-info>
+            <st-info-item label="订单号">{{info.order_id}}</st-info-item>
+            <st-info-item label="下单人">{{info.operator_name}}</st-info-item>
+            <st-info-item label="销售">{{info.staff_name}}</st-info-item>
+          </st-info>
+        </a-col>
+        <a-col :span="12" class="mgb-36">
+           <st-info>
+            <st-info-item label="下单时间">{{info.created_time}}</st-info-item>
+            <st-info-item label="订单总额">{{info.total_price}}元</st-info-item>
+            <st-info-item  label="实收金额">{{info.actual_price}}元</st-info-item>
+          </st-info>
+        </a-col>
+      </a-row>
+      <st-table
+      rowKey="staff_id"
+      :pagination="false"
+      :columns="columns"
+      :dataSource="info.split_items">
+        <template slot="staff_name" slot-scope="text, record">
+          <a-select
+            v-if="record.edit && record.staff_type !== 1"
+            @change="changeSaleMan($event, record)"
+            style="width: 150px"
+            placeholder="选择销售人员">
+              <a-select-option
+              v-for="item in saleList"
+              :key="item.id"
+              :value="item.id">{{item.staff_name}}</a-select-option>
+            </a-select>
+          <template v-else>{{text}}</template>
+        </template>
+        <template slot="split_ratio" slot-scope="text, record">
+          <st-input-number v-if="record.edit" :min="0" :max="100"
+          v-model="record.split_ratio" :float="true" placeholder="请输入占比" style="width:150px">
+            <span slot="addonAfter">%</span>
+          </st-input-number>
+          <template v-else>{{text}}</template>
+        </template>
+        <div slot="action" slot-scope="text, record, index">
+          <a v-if="record.edit === 3" @click="addSaleMan(record)">协助售卖人</a>
+          <a v-if="record.edit && record.edit !== 3" @click="onSave(record, index)">确定</a>
+          <a v-if="!record.edit" @click="onEidt(record, index)">编辑</a>
+          <a-divider type="vertical"></a-divider>
+          <a v-if="record.edit === 1 || record.edit === 2" @click="onCanel(record, index)">取消</a>
+          <a v-if="!record.edit && record.staff_type !== 1" @click="onDelete(index)">删除</a>
+        </div>
+      </st-table>
+      <div :class="basic('descrip')">
+        <label>备注</label>
+        <a-textarea v-model="description" :autosize="{ minRows: 4, maxRows: 6 }" />
+      </div>
+    </div>
+    <template slot="footer">
+      <st-button @click="onSubmit" :loading="loading.split" type="primary"> 确认拆分</st-button>
+    </template>
+  </st-modal>
+</template>
+
+<script>
+import moment from 'moment'
+import { SplitService } from './split.service'
+import { cloneDeep } from 'lodash-es'
+export default {
+  name: 'ModalShopFinanceOrderSplit',
+  bem: {
+    basic: 'modal-shop-finance-split'
+  },
+  serviceInject() {
+    return {
+      splitService: SplitService
+    }
+  },
+  rxState() {
+    return {
+      info: this.splitService.info$,
+      loading: this.splitService.loading$,
+      saleList: this.splitService.saleList$
+    }
+  },
+  props: ['id'],
+  data() {
+    return {
+      show: false,
+      description: '',
+      rate: 0,
+      saleMan: '',
+      form: this.$form.createForm(this),
+      columns: [
+        {
+          title: '销售姓名',
+          dataIndex: 'staff_name',
+          scopedSlots: { customRender: 'staff_name' }
+        }, {
+          title: '销售身份',
+          dataIndex: 'staff_type_name',
+          scopedSlots: { customRender: 'staff_type_name' }
+        }, {
+          title: '拆分比例',
+          dataIndex: 'split_ratio',
+          scopedSlots: { customRender: 'split_ratio' }
+        }, {
+          title: '业务金额(元)',
+          dataIndex: 'split_money',
+          scopedSlots: { customRender: 'split_money' }
+        }, {
+          title: '操作',
+          dataIndex: 'action',
+          scopedSlots: { customRender: 'action' }
+        }]
+    }
+  },
+  computed: {
+
+  },
+  created() {
+    this.splitService.serviceInit(this.id).subscribe(result => {
+      const item = {
+        edit: 3, // 1 主销售编辑 2 协助销售编辑 3协助销售新增
+        staff_id: -1,
+        staff_name: '',
+        staff_type: 2,
+        staff_type_name: '协助销售',
+        split_ratio: '',
+        split_money: ''
+      }
+      return this.info.split_items.unshift(item)
+    })
+  },
+  methods: {
+    changeSaleMan(event, record) {
+      this.saleList.forEach(item => {
+        if (item.id === event) {
+          record.staff_id = item.id
+          record.staff_name = item.staff_name
+        }
+      })
+    },
+    addSaleMan(record) {
+      if (!record.staff_id || !record.split_ratio) {
+        return
+      }
+      delete record.edit
+      record.split_money = ((+record.split_ratio / 100) * +this.info.actual_price).toFixed(1)
+      this.info.split_items.splice(0, 1, record)
+      this.info.split_items.unshift({
+        edit: 3,
+        staff_id: -1,
+        staff_name: '',
+        staff_type: 2,
+        staff_type_name: '协助销售',
+        split_ratio: '',
+        split_money: ''
+      })
+      delete record.saleMan
+    },
+    onSave(record, index) {
+      delete record.edit
+      record.split_money = ((+record.split_ratio / 100) * +this.info.actual_price).toFixed(1)
+      this.info.split_items.splice(index, 1, record)
+    },
+    onEidt(record, index) {
+      record.meta = cloneDeep(record)
+      record.edit = record.staff_type
+      this.info.split_items.splice(index, 1, record)
+    },
+    onCanel(record, index) {
+      record = record.meta
+      this.info.split_items.splice(index, 1, record)
+    },
+    onDelete(index) {
+      this.info.split_items.splice(index, 1)
+    },
+    moment,
+    onSubmit() {
+      const list = this.info.split_items.map(item => {
+        const params = {}
+        if (item.staff_id !== -1) {
+          params.staff_id = item.staff_id
+          params.staff_rate = item.split_ratio
+        }
+        return params
+      })
+      list.shift()
+      const params = {
+        order_id: this.id,
+        description: this.description,
+        split_data: list
+      }
+      this.splitService.split(params).subscribe(result => {
+        this.show = false
+        this.$emit('success')
+      })
+    }
+
+  }
+}
+</script>
