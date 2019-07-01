f2d6b9187cdebe5fe9a55daccd1ba7137362337f
liuyan <liuyan@styd.cn>
2019-07-01 18:15:10 +0800

    feat: 用户端设置

---
+++ src/views/pages/brand/setting/mina/components#/h5/coach.component.vue
@@ -0,0 +1,40 @@
+<template>
+  <div :class="action()">
+    <st-form-item label="模块名称">
+      <a-input placeholder="请输入模块名称" v-model="form.module_title" />
+    </st-form-item>
+    <st-form-item label="展示教练">
+      <a-row :gutter="12">
+        <a-col :span="4">
+            <div :class="action('addbox')">
+                + 添加教练
+            </div>
+        </a-col>
+        <a-col :span="4" v-for="(li, index) in form.staff_id_list" :key="index">
+          <div :class="action('box')">
+            <img :src="li.img">
+              <div>{{li.name}}</div>
+          </div>
+        </a-col>
+      </a-row>
+    </st-form-item>
+  </div>
+</template>
+<script>
+export default {
+  bem: {
+    action: 'coach-component'
+  },
+  data() {
+    return {
+      form: {
+        module_title: '',
+        staff_id_list: [{ name: '发放' }, { name: '大d' }, { name: '骄傲连' }]
+      }
+    }
+  },
+  methods: {
+  }
+}
+
+</script>
---
+++ src/views/pages/brand/setting/mina/components#/h5/course.component.vue
@@ -0,0 +1,67 @@
+<template>
+  <div :class="course()">
+    <st-form :form="form">
+        <div :class="course('bdbox')">
+      <st-t3 class="mg-y32">推荐私教课</st-t3>
+      <st-form-item label="模块名称">
+        <a-input placeholder="请输入模块名称"></a-input>
+      </st-form-item>
+      <st-form-item label="展示课程">
+        <a-select placeholder="请输入展示课程">
+          <a-select-option value="1">全部私教课</a-select-option>
+          <a-select-option value="2">展示可约天内排定私教课</a-select-option>
+          <a-select-option value="3">展示热门私教课</a-select-option>
+        </a-select>
+      </st-form-item>
+      </div>
+      <div :class="course('bdbox')">
+      <st-t3 class="mg-y32">推荐团体课</st-t3>
+      <st-form-item label="模块名称">
+        <a-input placeholder="请输入模块名称"></a-input>
+      </st-form-item>
+      <st-form-item label="展示课程">
+        <a-select placeholder="请输入展示课程">
+          <a-select-option value="2">展示可约天内的团体课</a-select-option>
+          <a-select-option value="3">展示热门团体课</a-select-option>
+        </a-select>
+      </st-form-item>
+      </div>
+      <div :class="course('bdbox')">
+      <st-t3 class="mg-y32">推荐课程包</st-t3>
+      <st-form-item label="模块名称">
+        <a-input placeholder="请输入模块名称"></a-input>
+      </st-form-item>
+      <st-form-item label="展示课程">
+        <a-select placeholder="请输入展示课程">
+          <a-select-option value="1">全部课程包</a-select-option>
+          <a-select-option value="2">展示热销课程包</a-select-option>
+          <a-select-option value="3">展示最新课程包</a-select-option>
+        </a-select>
+      </st-form-item>
+      </div>
+    </st-form>
+  </div>
+</template>
+<script>
+export default {
+  bem: {
+    course: 'course-component'
+  },
+  data() {
+    return {
+      form: {
+        module_title: '',
+        staff_id_list: [{
+          name: '发放'
+        }, {
+          name: '大d'
+        }, {
+          name: '骄傲连'
+        }]
+      }
+    }
+  },
+  methods: {}
+}
+
+</script>
