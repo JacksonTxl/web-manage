ea52a5dfc33067ae1ec0a8ef4bc33820b777ad45
JacksonTxl <172004133@qq.com>
2019-07-01 10:18:21 +0800

    poster

--- src/views/modals/brand/marketing/poster.vue
+++ src/views/modals/brand/marketing/poster.vue
@@ -3,26 +3,33 @@
     title="分享海报"
     v-model="show"
     @ok="handleOk"
+    width="400px"
     :footer="null"
   >
-    <div>
+    <div :class="basic()">
       <p>复制和下载二维码，推广至微信朋友圈、微博等，或打印活动海报，张贴至门店，吸引顾客参与活动。</p>
-      <div></div>
+      <img src=""/>
+      <div>
+        <st-button block pill type="primary" icon="download" size="large">下载海报</st-button>
+      </div>
     </div>
   </st-modal>
 </template>
 <script>
 export default {
-  name: 'BrandMarketingRuleTip',
+  name: 'BrandMarketingPoster',
+  bem: {
+    basic: 'brand-marketing-poster'
+  },
   serviceInject() {
     return {
     }
   },
   props: {
-    contents: {
-      type: Array,
+    type: {
+      type: String,
       default: () => {
-        return ['1、客户可以凭券抵扣相同面值的现金', '2、客户可以凭券抵扣相同面值的现金', '3、客户可以凭券抵扣相同面值的现金']
+        return 'http://www.baidu.com/'
       }
     }
   },
