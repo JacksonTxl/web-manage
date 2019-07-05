85e964968d9a5b0caa418c9f6975307646fcf708
tenglei <tenglei@rrr.me>
2019-07-05 10:59:29 +0800

    正则表达式

--- src/constants/rule.ts
+++ src/constants/rule.ts
@@ -7,22 +7,31 @@ export class RuleConfig {
   number = /^(\d+(\.\d?)?|\.\d?)$/ // 包含小数的数字  匹配 1.1 / 1. / .1 三种形式
   email = /^(\w)+(\.\w+)*@(\w)+((\.\w+)+)$/
 
-  generateRule(len = '1-20', opts: any = {}) {
-    const DEFAULT_OPTS = {
-      /**
-       * 中文、英文、数字
-       */
-      type: 1
-    }
-    opts = {
-      ...DEFAULT_OPTS,
-      ...opts
-    }
-    const REGEXP_MAP: any = {
-      1: '^[A-z0-9\\u4e00-\\u9fa5]{**}$'
-    }
-    const type = opts.type
-    const lens = len.split('-')
-    return new RegExp(REGEXP_MAP[type].replace('**', `${lens[0]},${lens[1]}`))
+  private REGEXP_MAP: RegExpMap = {
+    'CN_EN_NUM': '^[A-z0-9\\u4e00-\\u9fa5]{**}$'
   }
+  /**
+   * @param len
+   * @param type
+   * 示例 generateRule('4-16')
+   */
+  generateRule(len: string = '1-20', type: string = 'CN_EN_NUM') {
+    if (!/^\d+-\d+$/.test(len)) {
+      console.error(`请指定位数，例如'4-20'`)
+      return
+    }
+    if (!this.REGEXP_MAP.hasOwnProperty(type)) {
+      console.error(`类型${type}未定义`)
+      return
+    }
+    const lens = len.split('-')
+    return new RegExp(this.REGEXP_MAP[type].replace('**', `${lens[0]},${lens[1]}`))
+  }
+}
+
+export interface RegExpMap {
+  /**
+   * 中文、英文、数字，不含标点符号
+   */
+  [key: string]: string
 }
