5d84072097b1bbac0ec15f33cc45f497505bd4f3
FlynnLee <593029372@qq.com>
2019-07-09 10:48:44 +0800

    fix type error

--- src/views/modals/contract/constitution.service.ts
+++ src/views/modals/contract/constitution.service.ts
@@ -3,12 +3,11 @@ import {
   ContractConstitutionInput
 } from '@/api/v1/setting/contract'
 import { Injectable } from 'vue-service-app'
-import { Effect, State, Computed, log } from 'rx-state'
+import { Effect } from 'rx-state'
 import { Store } from '@/services/store'
 
 @Injectable()
 export class ConstitutionService extends Store<{}> {
-  state$ = new State({})
   constructor(private contractApi: ContractApi) {
     super()
   }
