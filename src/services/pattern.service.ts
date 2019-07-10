e1ae0895798a81fd75ab0f745900567fefdf3c9c
tenglei <tenglei@rrr.me>
2019-07-10 13:39:12 +0800

    update pattern

--- src/services/pattern.service.ts
+++ src/services/pattern.service.ts
@@ -5,6 +5,10 @@ export class PatternService {
      */
     'CN_EN_NUM': '^[A-z0-9\\u4e00-\\u9fa5]{**}$',
     /**
+     * 中文、英文、数字、空格
+     */
+    'CN_EN_NUM_SPACE': '^[A-z0-9\\u4e00-\\u9fa5][A-z0-9\\u4e00-\\u9fa5\\s]{**}[A-z0-9\\u4e00-\\u9fa5]$',
+    /**
      * 包含手机号和座机号
      */
     'TEL': '^(((\\+\\d{2}-)?0\\d{2,3}-\\d{7,8})|((\\+\\d{2}-)?(\\d{2,3}-)?([1][3,4,5,7,8][0-9]\\d{8})))$',
@@ -29,16 +33,38 @@ export class PatternService {
      */
   }
   /**
+   * 校验参数 len
+   */
+  private checkLen(len: string): boolean {
+    if (!/^\d+-\d+$/.test(len)) {
+      console.error(`请指定位数，例如'4-20'`)
+      return false
+    }
+    return true
+  }
+  private createPattern(patternName: string, len: string) {
+    if (!this.checkLen(len)) {
+      return
+    }
+    const lens = len.split('-')
+    return new RegExp(this.PATTERN_MAP[patternName].replace('**', `${lens[0]},${lens[1]}`))
+  }
+  /**
    * 中文、英文、数字，不含标点符号
    * @param len
    */
   CN_EN_NUM(len: string = '1-20') {
-    if (!/^\d+-\d+$/.test(len)) {
-      console.error(`请指定位数，例如'4-20'`)
-      return
-    }
+    return this.createPattern('CN_EN_NUM', len)
+  }
+  /**
+   * 中文、英文、数字、空格（中间，非开头与结尾）
+   */
+  CN_EN_NUM_SPACE(len: string = '1-20') {
     const lens = len.split('-')
-    return new RegExp(this.PATTERN_MAP['CN_EN_NUM'].replace('**', `${lens[0]},${lens[1]}`))
+    if (+lens[0] <= 2) {
+      return this.createPattern('CN_EN_NUM', len)
+    }
+    return this.createPattern('CN_EN_NUM_SPACE', `${+lens[0] - 2}-${+lens[1] - 2}`)
   }
   /**
    * 手机号与座机号
