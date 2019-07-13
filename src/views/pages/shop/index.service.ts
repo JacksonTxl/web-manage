a4f250117c7905b0085975267b0f0732cc19d03f
FlynnLee <593029372@qq.com>
2019-07-13 11:18:14 +0800

    update user.service

--- src/views/pages/shop/index.service.ts
+++ src/views/pages/shop/index.service.ts
@@ -1,31 +1,24 @@
 import {
   RouteGuard,
   Injectable,
-  ServiceRoute,
-  ServiceRouter
+  ServiceRoute
 } from 'vue-service-app'
 import { UserService } from '@/services/user.service'
-import { Computed } from 'rx-state'
-import { map } from 'rxjs/operators'
 
 @Injectable()
 export class IndexService implements RouteGuard {
-  firstMenu$ = new Computed(
-    this.userService.menuData$.pipe(
-      map((menuData: any) => {
-        return menuData.menus[0]
-      })
-    )
-  )
   constructor(
-    private userService: UserService,
-    private router: ServiceRouter
+    private userService: UserService
   ) {}
   beforeRouteEnter(to: ServiceRoute, from: ServiceRoute, next: any) {
     if (to.name === 'shop-index') {
-      this.router.replace({
-        name: this.firstMenu$.snapshot().url,
-        force: true
+      next({
+        name: this.userService.firstMenu$.snapshot().url,
+        query: {
+          _t: Math.random()
+            .toString(16)
+            .slice(3)
+        }
       })
     } else {
       next()
