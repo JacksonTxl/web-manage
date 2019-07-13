e9b237ad2a52ae21f2088d29b7f3a09a8db9aaaf
tenglei <tenglei@rrr.me>
2019-07-13 15:50:49 +0800

    储物柜fix

---
+++ src/views/layouts/entry#/fast-entry.config.ts
@@ -0,0 +1,42 @@
+import icon1 from '@/assets/img/fast-entry/1.png'
+import icon2 from '@/assets/img/fast-entry/2.png'
+// import icon3 from '@/assets/img/fast-entry/3.png'
+// import icon4 from '@/assets/img/fast-entry/4.png'
+import icon5 from '@/assets/img/fast-entry/5.png'
+// import icon6 from '@/assets/img/fast-entry/6.png'
+// import icon7 from '@/assets/img/fast-entry/7.png'
+// import icon8 from '@/assets/img/fast-entry/8.png'
+// import icon9 from '@/assets/img/fast-entry/9.png'
+
+export const entries = [{
+  icon: icon1,
+  text: '帮助中心'
+}, {
+  icon: icon2,
+  text: '在线咨询'
+},
+// {
+//   icon: icon3,
+//   text: '场馆列表'
+// }, {
+//   icon: icon4,
+//   text: '营销插件'
+// },
+{
+  icon: icon5,
+  text: '智能硬件'
+}
+// , {
+//   icon: icon6,
+//   text: '管理端App'
+// }, {
+//   icon: icon7,
+//   text: '数据中心'
+// }, {
+//   icon: icon8,
+//   text: '行业白皮书'
+// }, {
+//   icon: icon9,
+//   text: '小程序'
+// }
+]
