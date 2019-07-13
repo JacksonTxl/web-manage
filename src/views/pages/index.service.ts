e52378be583690112c35be57c073e2e5f4ac95c1
FlynnLee <593029372@qq.com>
2019-07-13 17:52:38 +0800

    to authService

--- src/views/pages/index.service.ts
+++ src/views/pages/index.service.ts
@@ -1,15 +1,15 @@
 import { RouteGuard, Injectable, ServiceRoute } from 'vue-service-app'
 import { UserService } from '@/services/user.service'
-import { RedirectService } from '@/services/redirect.service'
+import { AuthService } from '@/services/auth.service'
 
 @Injectable()
 export class IndexService implements RouteGuard {
   constructor(
     private userService: UserService,
-    private redirectService: RedirectService
+    private authService: AuthService
   ) {}
   beforeRouteEnter(to: ServiceRoute, from: ServiceRoute, next: any) {
-    this.redirectService.redirect({
+    this.authService.redirect({
       locateRouteName: 'index',
       redirectRouteName: this.userService.defaultRedirect$.snapshot(),
       to,
