bf2940edd88a73641d9277312d1df7d178046f39
tenglei <tenglei@rrr.me>
2019-07-17 14:57:43 +0800

    场地设置容纳人数修改为必填项，且限制输入人数为1-999

--- src/views/modals/court/add.vue
+++ src/views/modals/court/add.vue
@@ -2,13 +2,18 @@
   <st-modal title="创建场地" v-model="show" wrapClassName="modal-court-add" :footer="null">
     <st-form :form="form" labelWidth="68px" labelGutter="16px">
       <st-form-item label="场地名称" required>
-        <a-input placeholder="请输入场地名称，不超过10个字" maxlength="10" v-decorator="formRules.areaName"/>
+        <a-input placeholder="请输入场地名称，不超过10个字" maxlength="10" v-decorator="rules.areaName"/>
       </st-form-item>
       <st-form-item labelFix>
-        <a-checkbox v-decorator="formRules.isVip">VIP区域</a-checkbox>
+        <a-checkbox v-decorator="rules.isVip">VIP区域</a-checkbox>
       </st-form-item>
       <st-form-item label="容纳人数">
-        <st-input-number placeholder="请输入最大容纳人数，不填无限制" v-decorator="formRules.containNumber"/>
+        <st-input-number
+          placeholder="请输入最大容纳人数，1-999"
+          :min="1"
+          :max="999"
+          v-decorator="rules.containNumber"
+        />
       </st-form-item>
     </st-form>
     <div class="ta-r">
@@ -21,6 +26,8 @@
 import { AddService } from './add.service'
 import { MessageService } from '@/services/message.service'
 import { PatternService } from '@/services/pattern.service'
+import { rules } from './court.config'
+
 export default {
   serviceInject() {
     return {
@@ -36,24 +43,14 @@ export default {
   },
   data() {
     return {
-      show: false,
-      formRules: {
-        areaName: [
-          'area_name', {
-            rules: [{
-              required: true,
-              pattern: this.pattern.CN_EN_NUM('1-10'),
-              message: '支持输入中英文、数字,不超过10个字'
-            }]
-          }
-        ],
-        containNumber: ['contain_number'],
-        isVip: ['is_vip']
-      }
+      show: false
     }
   },
   created() {
     this.form = this.$form.createForm(this)
+  },
+  computed: {
+    rules
   },
   methods: {
     onSubmit(e) {
