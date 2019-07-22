caba52dc48942d8bec0adbb721798b70b76a15e0
wangjie <wangjie@styd.cn>
2019-07-22 10:58:11 +0800

    fix: 删除部分退款状态选项

--- src/views/pages/shop/staff/info/sold.vue
+++ src/views/pages/shop/staff/info/sold.vue
@@ -9,7 +9,6 @@
             <a-select-option :value="2">已完成</a-select-option>
             <a-select-option :value="3">已取消</a-select-option>
             <a-select-option :value="4">已退款</a-select-option>
-            <a-select-option :value="5">部分退款</a-select-option>
           </a-select>
           <a-range-picker class="mg-l8" @change="onChooseDate" format="YYYY-MM-DD"/>
         </a-col>
