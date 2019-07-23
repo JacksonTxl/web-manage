3d4e563d781953b343bebdd6f7b777eea498f990
张飞 <172004133@qq.com>
2019-07-23 17:13:28 +0800

    新增数统计

---
+++ src/views/pages/brand/statistics/info.vue
@@ -0,0 +1,45 @@
+<template>
+  <div :class="b()" >
+     <st-panel
+        :tabs="[
+          { label: '营收', route: { name: 'brand-statistics-info-revenue',query:{id:$route.query.id} } },
+          { label: '订单', route: { name: 'shop-member-info-follow-history',query:{id:$route.query.id} }},
+          { label: '用户', route: { name: 'shop-member-info-sold',query:{id:$route.query.id} } },
+          { label: '课程', route: { name: 'shop-member-info-reserve',query:{id:$route.query.id} } },
+          { label: '营销', route: { name: 'shop-member-info-sales-interests' ,query:{id:$route.query.id}} },
+        ]"
+    >
+      <router-view></router-view>
+    </st-panel>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BrandStatisticsInfo',
+  serviceInject() {
+    return {
+    }
+  },
+  rxState() {
+    return {
+    }
+  },
+  bem: {
+    b: 'page-brand-statistics'
+  },
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+
+  },
+  components: {
+  }
+}
+</script>
---
+++ src/views/pages/brand/statistics/statistics#/brand-shop.vue
@@ -0,0 +1,41 @@
+<template>
+  <div :class="basic()">
+    <a-radio-group @change="onChange" v-model="value">
+      <a-radio-button value="brand">品牌</a-radio-button>
+      <a-radio-button value="shop">门店</a-radio-button>
+    </a-radio-group>
+    <a-select defaultValue="lucy" style="width: 106px" @change="handleChange">
+      <a-select-option value="jack">全部</a-select-option>
+      <a-select-option value="lucy1">Lucy1</a-select-option>
+      <a-select-option value="lucy2">Lucy32</a-select-option>
+      <a-select-option value="lucy3">Lucy3</a-select-option>
+      <a-select-option value="lucy4">Lucy4</a-select-option>
+    </a-select>
+  </div>
+</template>
+<script>
+export default {
+  name: 'StatisticsBrandShop',
+  bem: {
+    basic: 'brand-statistics-brand-shop'
+  },
+  data() {
+    return {
+      value: 'brand'
+    }
+  },
+  methods: {
+    recentChange(query) {
+      this.$emit('change', query)
+    },
+    onChange(event) {
+      this.$emit('change', event)
+    },
+    handleChange(event) {
+      this.$emit('change', event)
+    }
+  },
+  components: {
+  }
+}
+</script>
