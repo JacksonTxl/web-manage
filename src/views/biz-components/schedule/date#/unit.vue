2ec16cda5828895e2a013adde62b81dcb720b46d
zhenglinxiong <zlx1433@gmail.com>
2019-07-23 15:29:48 +0800

    team Schedule course info

---
+++ src/views/biz-components/schedule/date#/unit.vue
@@ -0,0 +1,41 @@
+<template>
+  <li class="unit" @click="onClick">
+    <slot></slot>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'ScheduleUnit',
+  props: {
+    date: {
+      type: Object,
+      default: () => {
+        return {}
+      }
+    },
+    time: {
+      type: Number,
+      default: 0
+    }
+  },
+  computed: {
+    cDate() {
+      let tempTime = ''
+      let weekOfday = moment(this.date.date).format('E')
+      let tempDate = moment(this.date.date).subtract(weekOfday - this.date.week, 'days').format('YYYY-MM-DD')
+      if (this.time < 9) {
+        tempTime = `0${this.time}:00`
+      } else {
+        tempTime = `${this.time}:00`
+      }
+      return moment(`${tempDate} ${tempTime}`)
+    }
+  },
+  methods: {
+    onClick() {
+      this.$emit('change', this.cDate)
+    }
+  }
+}
+</script>
