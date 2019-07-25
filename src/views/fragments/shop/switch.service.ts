f25599092e881b302ee8764d0345c986c3e6cf06
FlynnLee <593029372@qq.com>
2019-07-25 14:56:15 +0800

    shopList to user.service

--- src/views/fragments/shop/switch.service.ts
+++ src/views/fragments/shop/switch.service.ts
@@ -1,7 +1,6 @@
 import { Injectable, ServiceRouter } from 'vue-service-app'
 import { State, Effect } from 'rx-state'
 import { tap, switchMap } from 'rxjs/operators'
-import { ShopApi } from '@/api/v1/shop'
 import { SwitchApi, SwitchShopInput } from '@/api/v1/account/switch'
 import { TokenService } from '@/services/token.service'
 import { UserService } from '@/services/user.service'
@@ -9,22 +8,14 @@ import { forkJoin } from 'rxjs'
 
 @Injectable()
 export class SwitchService {
-  shopList$ = new State([])
+  shopList$ = this.userService.shopList$
   loading$ = new State({})
   constructor(
-    private shopApi: ShopApi,
     private switchApi: SwitchApi,
     private tokenService: TokenService,
     private userService: UserService,
     private router: ServiceRouter
   ) {}
-  getShopList() {
-    return this.shopApi.getShopList().pipe(
-      tap(res => {
-        this.shopList$.commit(() => res.list)
-      })
-    )
-  }
   reloadMenuAndUser() {
     return forkJoin([
       this.userService.getMenuData(),
