38e52733b752e75576b5f435eb8512e0f1a9781a
liyue <1084137410@qq.com>
2019-07-30 10:09:16 +0800

    加图片预览

--- src/views/pages/brand/setting/sms/notice.vue
+++ src/views/pages/brand/setting/sms/notice.vue
@@ -82,6 +82,7 @@ export default {
     },
     putNotice(para) {
       return this.noticeService.putNotice({ ...para }).subscribe(res => {
+        this.$message.success('编辑成功')
         this.getNoticeList()
       })
     }
