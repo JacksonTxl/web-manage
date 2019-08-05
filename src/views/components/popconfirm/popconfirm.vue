c9c6d1216b4dd30c80bcd43aa12b2f4ee2f7b576
FlynnLee <593029372@qq.com>
2019-08-05 14:12:36 +0800

    add st-popconfirm

---
+++ src/views/components/popconfirm/popconfirm.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: 'StPopconfirm',
+  render(h) {
+    const props = {
+      title: '是否确认删除',
+      okText: '确定',
+      cancelText: '取消',
+      ...this.$attrs
+    }
+    return h(
+      'a-popconfirm',
+      {
+        props,
+        on: this.$listeners,
+        scopedSlots: this.$scopedSlots,
+        slot: this.$slots
+      },
+      this.$slots.default
+    )
+  }
+}
+</script>
