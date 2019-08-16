a72ca62cb5ff1eb3e77572fd0d03811a70a67310
FlynnLee <593029372@qq.com>
2019-08-16 09:41:09 +0800

    fix: add dateFilter && weekFilter

--- src/filters/date.filters.ts
+++ src/filters/date.filters.ts
@@ -3,6 +3,19 @@ import moment from 'moment'
 /**
  * 日期处理过滤器集合
  */
-export const dateFilter = (key: string | number, format: string) => {
+export const dateFilter = (
+  key: string | number,
+  format: string,
+  defaultValue: string = ''
+) => {
+  if (!key) {
+    return defaultValue
+  }
   return moment(key).format(format)
+}
+/**
+ * @param key 根据索引返回周几
+ */
+export const weekFilter = (key: number) => {
+  return ['周日', '周一', '周二', '周三', '周四', '周五', '周六'][key]
 }
