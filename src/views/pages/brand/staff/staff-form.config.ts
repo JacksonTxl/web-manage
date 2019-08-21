a71ced29e4999d8d5c187c780cd1557ea227e4ed
“zlx” <1186641583@qq.com>
2019-08-21 19:16:20 +0800

    fix(1006359): 【品牌/门店——新增员工——基础信息】员工开通账号后密码输入不一致，应该由前端校验密码是否一致

--- src/views/pages/brand/staff/staff-form.config.ts
+++ src/views/pages/brand/staff/staff-form.config.ts
@@ -40,12 +40,17 @@ export const ruleOptions = (vm: any) => {
     repeat_password: {
       rules: [
         {
-          required: true,
-          message: '请再次输入登录密码'
-        },
-        {
-          pattern: new RegExp(/^.{6,15}$/),
-          message: '请输入6~15个字符'
+          validator: (field: any, value: any, values: any) => {
+            if (!value) {
+              return '请输入登录密码'
+            }
+            if (value.length < 6 || value.length > 15) {
+              return '请输入正确格式登录密码'
+            }
+            if (value !== values.password) {
+              return '两次密码输入不一致'
+            }
+          }
         }
       ]
     },
