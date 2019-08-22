6acd5dd4ddac080acb296d99b5f74aa1e2e318ec
“zlx” <1186641583@qq.com>
2019-08-22 10:48:28 +0800

    fix(1006358): 【品牌/门店——新增员工——基础信息】姓名输入框输入一个字符报错

--- src/views/pages/shop/staff/staff-form.config.ts
+++ src/views/pages/shop/staff/staff-form.config.ts
@@ -5,11 +5,11 @@ export const ruleOptions = (vm: any) => {
       rules: [
         {
           required: true,
-          message: '请输入登录账户'
+          message: '请输入姓名'
         },
         {
-          pattern: pattern.CN_EN_NUM('6-15'),
-          message: '请输入6~15d个字符'
+          pattern: pattern.CN_EN_NUM('0-15'),
+          message: '请输入15个字符以内'
         }
       ]
     },
