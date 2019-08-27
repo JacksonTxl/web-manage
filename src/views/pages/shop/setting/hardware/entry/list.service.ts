6336a6da5916395760785968336990db1618c492
liyue <liyue@liyuedeMacBook-Pro.local>
2019-08-27 14:47:01 +0800

    fix: 修改权限

--- src/views/pages/shop/setting/hardware/entry/list.service.ts
+++ src/views/pages/shop/setting/hardware/entry/list.service.ts
@@ -32,6 +32,7 @@ export class ListService {
   getAreaList(query: GetAreaListQuery) {
     return this.HareWareApi.getAreaList(query).pipe(
       tap((res: any) => {
+        res = this.authService.filter(res)
         this.list$.commit(() => res.list)
         this.page$.commit(() => res.page)
       })
