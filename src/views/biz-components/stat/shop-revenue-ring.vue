79dde335dd2696e6e97b7f60896ddacd36358b79
“zlx” <1186641583@qq.com>
2019-08-28 10:33:57 +0800

    fix(1006039): 【UI-数据统计】3

--- src/views/biz-components/stat/shop-revenue-ring.vue
+++ src/views/biz-components/stat/shop-revenue-ring.vue
@@ -87,7 +87,7 @@ export default {
       this.chart.guide().html({
         position: ['50%', '50%'],
         html: () => {
-          const sum = this.sum
+          const sum = this.sum.toFixed(1)
           return (
             `<div class='guide'>` +
             `<div class='guide-title'><span class='guide-value'>${sum}</span><span class='guide-unit'>${
