afcbb7b1d2668896faae40aed52d530cd4693667
FlynnLee <593029372@qq.com>
2019-08-29 18:15:24 +0800

    style: no skeleton

--- src/views/layouts/index.ts
+++ src/views/layouts/index.ts
@@ -1,12 +1,10 @@
 import LayoutBlank from './blank.vue'
 import LayoutDefault from './default.vue'
-// import LayoutSkeleton from './skeleton.vue'
 import LayoutLoading from './loading.vue'
 
 // this is will used in router/routes
 export const layoutMap = {
   blank: LayoutBlank,
   default: LayoutDefault,
-  // skeleton: LayoutSkeleton,
   loading: LayoutLoading
 }
