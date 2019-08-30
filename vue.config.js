1833b6942a903bfc77dab2e30574ca3d101f945d
FlynnLee <593029372@qq.com>
2019-08-30 18:14:57 +0800

    fix: 404 add chunk-vendors

--- vue.config.js
+++ vue.config.js
@@ -60,7 +60,7 @@ module.exports = {
       entry: 'src/_modules/404/index.ts',
       template: 'src/_modules/404/index.html',
       filename: '404/index.html',
-      chunks: ['chunk-common', 'runtime~404', '404']
+      chunks: ['chunk-vendors', 'chunk-common', 'runtime~404', '404']
     }
   },
   lintOnSave: false,
