b8b4cf0d7267a39a402e517367589ed46e2bdae8
FlynnLee <593029372@qq.com>
2019-09-19 15:07:39 +0800

    fix: hm.js on every enter

--- src/services/track.service.ts
+++ src/services/track.service.ts
@@ -1,20 +1,19 @@
 import { RouteGuard, ServiceRoute, Injectable } from 'vue-service-app'
 import { AppConfig } from '@/constants/config'
+import { ImportService } from '@/services/import.service'
 
 @Injectable()
 export class TrackService implements RouteGuard {
-  constructor(private appConfig: AppConfig) {}
-  init() {
-    // 只在生产域名下才使用百度统计
+  constructor(
+    private appConfig: AppConfig,
+    private importService: ImportService
+  ) {}
+
+  beforeRouteEnter() {
     if (this.appConfig.HOST_IS_PROD) {
-      const hm = document.createElement('script')
-      hm.src = 'https://hm.baidu.com/hm.js?11c0b426fbd4b0e05de4ee2084df1835'
-      hm.defer = true
-      const s = document.getElementsByTagName('script')[0]
-      s.parentNode && s.parentNode.insertBefore(hm, s)
+      this.importService.loadJs(
+        'https://hm.baidu.com/hm.js?11c0b426fbd4b0e05de4ee2084df1835'
+      )
     }
-  }
-  beforeRouteEnter(to: ServiceRoute, from: ServiceRoute) {
-    this.init()
   }
 }
