9ba97f86ca30eb95c3751c5a6cda00b2805033c5
“zlx” <1186641583@qq.com>
2019-09-20 14:59:54 +0800

    feat: 预约管理

--- src/views/pages/shop/reception/reserve.vue
+++ src/views/pages/shop/reception/reserve.vue
@@ -45,6 +45,7 @@
         @click="onAddReverse"
         type="primary"
         class="shop-member-list-button"
+        v-show="query.reserve_type === 2"
         icon="add"
       >
         到访预约
