35ffaf2ad22464efb537971af4ad145471d51e25
FlynnLee <593029372@qq.com>
2019-09-20 14:16:28 +0800

    feat: add form-member-search

---
+++ src/views/biz-components/member-search/member-search.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from 'vue-service-app'
+import { TransactionApi } from '@/api/v1/sold/transaction'
+import { Effect, State, Action, Computed } from 'rx-state'
+import { then } from '@/operators'
+import { debounceTime, switchMap } from 'rxjs/operators'
+
+@Injectable()
+export class MemberSearchService {
+  memberList$ = new State([])
+  loading$ = new State({})
+  type$ = new State('')
+
+  constructor(private transactionApi: TransactionApi) {
+    this.getMemberAction$ = new Action(action$ =>
+      action$.pipe(
+        debounceTime(500),
+        switchMap(searchText => this.getMemberRequest(searchText))
+      )
+    )
+  }
+  SET_TYPE(type: string) {
+    this.type$.commit(() => type)
+  }
+  RESET_LIST() {
+    this.memberList$.commit(() => [])
+  }
+  getTransactionMember(member: string, type: number) {
+    return this.transactionApi.getMemberList(member, type).pipe(
+      then((res: any) => {
+        this.memberList$.commit(() => res.list)
+        return res.list
+      })
+    )
+  }
+  @Effect()
+  getMemberRequest(searchText: string) {
+    switch (this.type$.snapshot()) {
+      case 'transaction':
+        return this.getTransactionMember(searchText, 1)
+      default:
+        throw new Error('member-search.service should pass type')
+    }
+  }
+  getMember(searchText: string) {
+    this.getMemberAction$.dispatch(searchText)
+  }
+}
