a3a1eec2d6d901614a54f72c47a6579372e8b0ce
FlynnLee <593029372@qq.com>
2019-09-20 17:19:10 +0800

    fix: 修复退款金额大于订单总额

--- src/views/biz-modals/sold/course/refund.config.ts
+++ src/views/biz-modals/sold/course/refund.config.ts
@@ -1,7 +1,16 @@
 export const ruleOptions = (vm: any) => {
   return {
     refundPrice: {
-      rules: [{ required: true, message: '请输入退款金额' }]
+      rules: [
+        { required: true, message: '请输入退款金额' },
+        {
+          validator: (field: any, value: number) => {
+            if (Number(value) > Number(vm.packageInfo.total_price)) {
+              return '退款金额不能大于订单总额'
+            }
+          }
+        }
+      ]
     }
   }
 }
