f7b00569a966df2566fb540206971e924ee882d2
liyue <liyue@liyuedeMBP.lan>
2019-09-21 11:03:28 +0800

    fix: 1007434 【品牌——营销插件——幸运大转盘】奖品设置页：编辑控件设置奖品数量和中奖概率而非奖品弹窗

--- src/views/pages/brand/marketing/plugin/lottery/add.vue
+++ src/views/pages/brand/marketing/plugin/lottery/add.vue
@@ -334,10 +334,26 @@
                           : item.support_shop_ids.length
                       }}
                     </td>
-                    <td>
-                      {{ item.number }}
+                    <td @click="editTableIndexNum(index)">
+                      <st-input-number
+                        :min="1"
+                        :max="99999"
+                        :step="1"
+                        :precision="0"
+                        :value="item.number"
+                        :index="index"
+                        @change="editTableNum"
+                      ></st-input-number>
                     </td>
-                    <td>{{ item.rate }}</td>
+                    <td @click="editTableIndexRate(index)">
+                      <st-input-number
+                        :min="0"
+                        :max="100"
+                        :float="true"
+                        :value="item.rate"
+                        @change="editTableRate"
+                      ></st-input-number>
+                    </td>
                     <td>
                       <st-table-actions>
                         <a
@@ -460,10 +476,11 @@ export default {
         '奖品类型',
         '可用门店（家）',
         '奖品数量（个）',
-        '中奖概率',
+        '中奖概率 (%)',
         '操作'
       ],
       info: {},
+      tableIndex: 0,
       preview: {
         title: '',
         startTime: '',
@@ -689,6 +706,18 @@ export default {
       this.prizeList = this.prizeList.filter((item, index) => {
         return index !== para
       })
+    },
+    editTableIndexNum(index) {
+      this.tableIndex = index
+    },
+    editTableNum(val) {
+      this.prizeList[this.tableIndex].number = val
+    },
+    editTableIndexRate(index) {
+      this.tableIndex = index
+    },
+    editTableRate(val) {
+      this.prizeList[this.tableIndex].rate = val
     },
     editVIew(id) {
       return this.addService.editVIew(id).subscribe(res => {
