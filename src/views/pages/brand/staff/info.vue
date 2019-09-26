2216d4568d219e0c07c241418b9334173ce1207b
flowMemory <zhengtengfei@styd.cn>
2019-09-26 21:34:14 +0800

    fix(1007208): 【UI-下拉选择框】

--- src/views/pages/brand/staff/info.vue
+++ src/views/pages/brand/staff/info.vue
@@ -27,9 +27,9 @@
               </span>
             </div>
             <div class="staff-info-shops">
-              <span class="mg-r4">所在部门：{{ info.department }}</span>
-              <span class="line">|</span>
-              <span class="mg-l4">所在门店: {{ info.shop_num }}家门店</span>
+              <span class="mg-r8">所在部门：{{ info.department }}</span>
+              <span class="line"></span>
+              <span class="mg-l8">所在门店: {{ info.shop_num }}家门店</span>
             </div>
           </div>
         </a-col>
