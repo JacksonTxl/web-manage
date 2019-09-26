0fce4b0251346aa7308078a656d5e2b81c201723
FlynnLee <593029372@qq.com>
2019-09-26 09:58:28 +0800

    fix(card/member): 1007749 【门店/品牌——经营类目】新增会员卡售卖方式未修改

--- src/views/pages/brand/product/card/member/period/add.vue
+++ src/views/pages/brand/product/card/member/period/add.vue
@@ -526,7 +526,6 @@
                   <a-checkbox
                     v-for="item in sell_type_list"
                     :key="item.value"
-                    :disabled="item.value === SELL_TYPE.OFFLINE"
                     :value="item.value"
                   >
                     {{ item.label }}
