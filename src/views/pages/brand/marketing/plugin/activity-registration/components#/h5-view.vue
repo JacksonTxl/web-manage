11dc72819291b5d719034191a3242039dc99705b
“zlx” <1186641583@qq.com>
2019-10-11 11:21:21 +0800

    feat: 活动报名表单

--- src/views/pages/brand/marketing/plugin/activity-registration/components#/h5-view.vue
+++ src/views/pages/brand/marketing/plugin/activity-registration/components#/h5-view.vue
@@ -5,8 +5,60 @@
         活动报名
       </template>
       <div :class="bComponent('content')">
-        <img src="" alt="" />
-        <div class="title"></div>
+        <div :class="bComponent('content-img')">
+          <img src="" alt="" />
+        </div>
+        <div :class="bComponent('content-info')">
+          <st-t2 class="title">{{ stepInfo.name }}</st-t2>
+          <div class="content">
+            <div class="price-info">
+              <span class="price">¥4000</span>
+              <span class="subfix">起</span>
+            </div>
+            <div class="hot-info">
+              <span class="view">
+                190
+              </span>
+              <span class="share">
+                108
+              </span>
+            </div>
+          </div>
+        </div>
+        <div :class="bComponent('content-sub-info')">
+          <div class="time">
+            <st-icon type="timer" class="mg-r16"></st-icon>
+            <span>2019.06.22 19:30-21:30</span>
+          </div>
+          <div class="address">
+            <div class="address__info">
+              <st-icon type="location" class="mg-r16"></st-icon>
+              <span>上海市闵行区漕河泾门店</span>
+            </div>
+            <div class="address__map"></div>
+          </div>
+        </div>
+        <div :class="bComponent('content-activity-detail')">
+          <st-t2 class="title">活动详情</st-t2>
+          <div class="content-box">
+            <p></p>
+            打造迷人身线，拥抱健康身材。 测量您的身体成分，了解现有身体状况。
+            <p></p>
+            根据您的身体状况，量身定制科学减脂计划。
+            <p></p>
+            1对1授课，帮您高效减脂，改善体态，重建良好身体机能。
+            <p></p>
+            根据您的运动计划，制定不同周期饮食参考，监督每日饮食。
+            <p></p>
+            注：2月26日前购买的消费者仍按原有套餐内容接待。
+            <p></p>
+            漕河泾店营业时间为8：00-22：00； 漕溪路店营业时间为10：00-22：00。
+          </div>
+        </div>
+        <div :class="bComponent('footer')">
+          <st-icon size="44px" type="edit"></st-icon>
+          <st-button class="footer__button" type="primary">立即报名</st-button>
+        </div>
       </div>
     </h5-container>
   </div>
@@ -17,7 +69,7 @@ import H5Container from '@/views/biz-components/h5/h5-container'
 export default {
   name: 'h5-view',
   bem: {
-    bComponent: 'activity-h5-view'
+    bComponent: 'component-activity-h5-view'
   },
   components: {
     H5Container
--- src/views/pages/brand/marketing/plugin/activity-registration/components#/step1-form.vue
+++ src/views/pages/brand/marketing/plugin/activity-registration/components#/step1-form.vue
@@ -4,10 +4,31 @@
       <a-input v-decorator="decorators.name"></a-input>
     </st-form-item>
     <st-form-item label="活动时间" required>
-      <a-input v-decorator="decorators.count"></a-input>
+      <a-range-picker />
     </st-form-item>
     <st-form-item label="活动地点" required>
-      <a-input v-decorator="decorators.address"></a-input>
+      <map-button></map-button>
+    </st-form-item>
+    <st-form-item label="活动海报" required>
+      <st-image-upload
+        width="96px"
+        height="96px"
+        :list="fileShareList"
+        @change="onShareChangeGetAvatar"
+        placeholder="上传图片"
+      ></st-image-upload>
+      <span>
+        请上传jbg、png格式的图片
+      </span>
+    </st-form-item>
+    <st-form-item label="活动人数" required>
+      <a-input
+        placeholder="若不限制活动人数，请填写0"
+        v-decorator="decorators.address"
+      ></a-input>
+    </st-form-item>
+    <st-form-item label="活动详情" required>
+      <st-editor></st-editor>
     </st-form-item>
     <!-- <st-form-item label="活动海报" required>
       <a-input></a-input>
@@ -24,6 +45,8 @@
 <script>
 import { ruleOptions } from './form.config'
 import { PatternService } from '@/services/pattern.service'
+import MapButton from '@/views/biz-components/map-button/map-button'
+import StEditor from '@/views/biz-components/editor/editor'
 export default {
   name: 'Step1Form',
   serviceInject() {
@@ -35,14 +58,27 @@ export default {
     const that = this
     const form = this.$stForm.create({
       onValuesChange(props, values) {
-        that.$emit('change', that.form.getFieldsValue())
+        console.log(values)
+        that.$nextTick().then(() => {
+          that.$emit('change', that.form.getFieldsValue())
+        })
       }
     })
     const decorators = form.decorators(ruleOptions)
     return {
       form,
       formInfo: {},
-      decorators
+      decorators,
+      fileShareList: []
+    }
+  },
+  components: {
+    MapButton,
+    StEditor
+  },
+  methods: {
+    onShareChangeGetAvatar(imageFiles) {
+      this.fileShareList = cloneDeep(imageFiles)
     }
   }
 }
