f0e41e147893ae53d015920eb496fd48888f53af
liyue <liyue@liyuedeMacBook-Pro.local>
2019-10-11 17:37:33 +0800

    fix: 找回丢失代码

--- src/views/pages/brand/marketing/plugin/lottery/add.vue
+++ src/views/pages/brand/marketing/plugin/lottery/add.vue
@@ -35,59 +35,18 @@
               <img class="img" :src="item.prize.image_url" alt="奖品图片" />
               <div class="text">{{ item.prize_name }}</div>
             </div>
-            <img
-              :class="bPage('lottery-banner')"
-              src="~@/assets/img/brand/marketing/lottery/bg.png"
-            />
-            <img
-              :class="bPage('lottery-title-bg')"
-              src="~@/assets/img/brand/marketing/lottery/title_bg.png"
-            />
-            <span :class="bPage('lottery-subtitle')">{{ preview.title }}</span>
-            <div :class="bPage('lottery-turntable')">
-              <div style="position:relative" v-if="prizeList.length > 0">
-                <div
-                  class="img-wrap"
-                  :class="'run-item-' + index"
-                  v-for="(item, index) in prizeList"
-                  :key="index"
-                >
-                  <img class="img" :src="item.prize.image_url" alt="奖品图片" />
-                  <div class="text">{{ item.prize_name }}</div>
-                </div>
-              </div>
-              <div v-if="notPrize.prize_name" class="img-wrap run-item-7">
-                <div class="img">
-                  <img
-                    style="width: 100%;"
-                    :src="
-                      notPrizeImgType === NOT_PRIZE_IMG_TYPE.CUSTOM
-                        ? notPrize.prize.image_url
-                        : lucky[0].image_url
-                    "
-                    alt="奖品图片"
-                  />
-                </div>
-                <div class="text">{{ notPrize.prize_name }}</div>
-              </div>
-            </div>
-            <div :class="bPage('lottery-footer')">
-              <div :class="bPage('lottery-title')">活动规则</div>
-              <div class="mg-b24">
-                <span class="mg-r8">活动时间:</span>
-                <span>{{ preview.startTime }}--{{ preview.endTime }}</span>
-              </div>
-              <div class="mg-b24">
-                <span class="mg-r8">抽奖规则:</span>
-                <span v-if="timesType === 1">
-                  每人每天有{{ preview.perTimes }}次机会
-                </span>
-                <span v-else>每人总共有{{ preview.totalTimes }}次机会</span>
-              </div>
-              <div class="mg-b24">
-                <span class="mg-r8">活动说明:</span>
-                <span>{{ preview.description }}</span>
-              </div>
+          </div>
+          <div v-if="notPrize.prize_name" class="img-wrap run-item-7">
+            <div class="img">
+              <img
+                style="width: 100%;"
+                :src="
+                  notPrizeImgType === NOT_PRIZE_IMG_TYPE.CUSTOM
+                    ? notPrize.prize.image_url
+                    : lucky[0].image_url
+                "
+                alt="奖品图片"
+              />
             </div>
             <div class="text">{{ notPrize.prize_name }}</div>
           </div>
@@ -112,7 +71,6 @@
         </div>
       </div>
     </h5-container>
-
     <div :class="bPage('form')">
       <Steps :value="currentIndex" @skip="next" :stepArr="stepArr" />
       <div style="padding:24px;">
