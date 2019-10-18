e422420f1e50621ff1c4cbc9f10238860c53d9b1
liyue <liyue@liyuedeMacBook-Pro.local>
2019-10-18 11:57:10 +0800

    fix: 修改字段

---
+++ src/constants/sold/usage.ts
@@ -0,0 +1,5 @@
+export const enum USAGE_TYPES {
+  ENTRANCE = 1,
+  ORDER = 2,
+  CANCEL = 3
+}
--- src/views/pages/shop/sold/card/info/member/info/usage-log.vue
+++ src/views/pages/shop/sold/card/info/member/info/usage-log.vue
@@ -8,38 +8,35 @@
       @change="onTableChange"
       :dataSource="list"
     >
-      <template slot="operate_object" slot-scope="text, record">
-        <span v-if="text.length === 0">{{ record.object }}</span>
-        <div v-else>
-          <a-popover title="操作对象">
-            <template slot="content">
-              <pre>{{ record.object }}</pre>
-            </template>
-            <a class="pop-object__text">{{ text }}</a>
-          </a-popover>
+      <template slot="usage_type" slot-scope="text, record">
+        <span>
+          {{
+            record.usage_type === USAGE_TYPES.ENTRANCE
+              ? '入场'
+              : record.usage_type === USAGE_TYPES.ORDER
+              ? '预约团体课'
+              : '取消预约团体课'
+          }}
+        </span>
+        <div>
+          <span>课程名称:</span>
+          <span>{{ record.course_name }}</span>
+        </div>
+        <div>
+          <span>上课时间:</span>
+          <span>
+            {{ record.course_start_time }}-- {{ record.course_end_time }}
+          </span>
+        </div>
+        <div>
+          <span>上课教练:</span>
+          <span>{{ record.coach_name }}</span>
         </div>
       </template>
-      <template slot="before_operate" slot-scope="text, record">
-        <span v-if="text.length === 0">{{ record.before }}</span>
-        <div v-else>
-          <a-popover title="操作前">
-            <template slot="content">
-              <pre>{{ record.before }}</pre>
-            </template>
-            <a class="pop__text">{{ text }}</a>
-          </a-popover>
-        </div>
-      </template>
-      <template slot="after_operate" slot-scope="text, record">
-        <span v-if="text.length === 0">{{ record.after }}</span>
-        <div v-else>
-          <a-popover title="操作后">
-            <template slot="content">
-              <pre>{{ record.after }}</pre>
-            </template>
-            <a class="pop__text">{{ text }}</a>
-          </a-popover>
-        </div>
+      <template slot="amount_change" slot-scope="text, record">
+        <span :style="{ color: record.amount_change < 0 ? 'red' : 'green' }">
+          {{ record.amount_change }}
+        </span>
       </template>
     </st-table>
   </section>
@@ -48,6 +45,8 @@
 import { UsageLogService } from './usage-log.service'
 import { RouteService } from '@/services/route.service'
 import { columns } from './usage-log.config'
+import { USAGE_TYPES } from '@/constants/sold/usage'
+
 import tableMixin from '@/mixins/table.mixin'
 export default {
   name: 'PageShopSoldCardMemberInfoOperation',
@@ -67,6 +66,11 @@ export default {
       list: this.usageLogService.list$,
       loading: this.usageLogService.loading$,
       query: this.routeService.query$
+    }
+  },
+  data() {
+    return {
+      USAGE_TYPES
     }
   },
   computed: {
