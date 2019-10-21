9da4d927ee01b0923024f50d2f6af84a05fd3ad2
liyue <liyue@liyuedeMacBook-Pro.local>
2019-10-21 19:02:26 +0800

    fix: 修改指令写法

--- src/directives/export.directive.ts
+++ src/directives/export.directive.ts
@@ -3,32 +3,17 @@ import { ExportService } from '@/services/export.service'
 import Vue from 'vue'
 import { Modal } from 'ant-design-vue'
 
-Vue.directive('export-excel', {
-  bind: function(el, val, vnode) {
-    const exportService = container.get(ExportService)
-    if (!val.value.type) {
-      return
-    }
-    el.onclick = function() {
-      Modal.confirm({
-        title: '是否确认导出?',
-        onOk: function() {
-          exportService.export(val.value.type, val.value.query).subscribe()
-        }
-      })
-    }
+Vue.directive('export-excel', function(el, val, vnode) {
+  const exportService = container.get(ExportService)
+  if (!val.value.type) {
+    throw new Error('error: type is necessary')
+  }
+  el.onclick = function() {
+    Modal.confirm({
+      title: '是否确认导出?',
+      onOk: function() {
+        exportService.export(val.value.type, val.value.query).subscribe()
+      }
+    })
   }
 })
-// export const exportExcel = () => {
-//   bind: (el: any, val: any, vnode: any) => {
-//     const exportService = container.get(ExportService)
-//     el.onclick = function() {
-//       Modal.confirm({
-//         title: '是否确认导出?',
-//         onOk: function() {
-//           exportService.export(val.value.type, val.value.para).subscribe()
-//         }
-//       })
-//     }
-//   }
-// }
