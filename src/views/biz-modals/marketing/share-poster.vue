420c21d9a671780dd4a27cec888e25880fa1bfc1
“zlx” <1186641583@qq.com>
2019-10-21 15:54:13 +0800

    feat: 活动报名

---
+++ src/views/biz-modals/marketing/share-poster.vue
@@ -0,0 +1,22 @@
+<template>
+  <st-modal title="分享海报" :footer="null" width="500px" v-model="show">
+    <p>
+      复制或下载二维码，推广至微信朋友圈、微博等，或打印活动海报，张贴至门店，吸引顾客参与活动。
+    </p>
+    <div></div>
+    <st-button icon="download" type="primary" pill>下载海报</st-button>
+  </st-modal>
+</template>
+<script>
+export default {
+  name: 'ModalSharePoster',
+  bem: {
+    bModal: 'modal-marketing-add-ticket'
+  },
+  data() {
+    return {
+      show: false
+    }
+  }
+}
+</script>
