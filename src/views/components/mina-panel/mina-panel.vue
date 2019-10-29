6174d1162d9c3edd244bacd8b4901c49570d6e4c
FlynnLee <593029372@qq.com>
2019-10-29 11:15:32 +0800

    fix(st-mina-panel): 更新为min-height 移除scroll监听器

--- src/views/components/mina-panel/mina-panel.vue
+++ src/views/components/mina-panel/mina-panel.vue
@@ -3,6 +3,10 @@
     <div :class="b('preview')">
       <slot name="preview"></slot>
     </div>
+    <a-row>
+      <a-col :xl="12"></a-col>
+      <a-col :xl="8"></a-col>
+    </a-row>
     <div :class="b('main')">
       <slot name="prepend"></slot>
       <div :class="b('content')">
@@ -29,22 +33,31 @@ export default {
   },
   data() {
     return {
-      isActionFixed: true
+      isActionFixed: true,
+      footerEl: null
+    }
+  },
+  methods: {
+    handleActionsPosition() {
+      const rect = this.footerEl.getBoundingClientRect()
+
+      // 视窗内
+      if (rect.top < window.innerHeight) {
+        this.isActionFixed = false
+      } else {
+        this.isActionFixed = true
+      }
     }
   },
   mounted() {
-    const footerEl = document.querySelector('.layout-default-body__footer')
-    if (footerEl)
-      window.addEventListener('scroll', () => {
-        const rect = footerEl.getBoundingClientRect()
-
-        // 视窗内
-        if (rect.top < window.innerHeight) {
-          this.isActionFixed = false
-        } else {
-          this.isActionFixed = true
-        }
-      })
+    this.footerEl = document.querySelector('.layout-default-body__footer')
+    if (this.footerEl) {
+      this.handleActionsPosition()
+      window.addEventListener('scroll', this.handleActionsPosition, false)
+    }
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.handleActionsPosition, false)
   }
 }
 </script>
