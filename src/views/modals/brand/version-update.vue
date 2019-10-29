31adbbd64e1c4487af1a34bf6122475168c9994a
张飞 <172004133@qq.com>
2019-10-29 11:42:53 +0800

    fix: 111

--- src/views/modals/brand/version-update.vue
+++ src/views/modals/brand/version-update.vue
@@ -1,5 +1,5 @@
 <template>
-  <st-modal
+  <!-- <st-modal
     title="功能未开放"
     v-model="show"
     @close="onClose"
@@ -8,11 +8,27 @@
     :maskClosable="true"
   >
     需要升级到俱乐部版本才能使用
-  </st-modal>
+  </st-modal> -->
+  <div :class="b()" v-if="show">
+    <div :class="b('bg')"></div>
+    <div :class="b('content')">
+      <p :class="b('title')">俱乐部版本专属功能</p>
+      <p :class="b('desc')">
+        升级到俱乐部版本，解锁更多功能
+        <br />
+        请咨询三体云动工作人员
+      </p>
+      <p :class="b('phone')">400-962-8988</p>
+      <p :class="b('time')">服务时间：09:00 - 18:00 (工作日)</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
+  bem: {
+    b: 'version-update'
+  },
   data() {
     return {
       show: false
