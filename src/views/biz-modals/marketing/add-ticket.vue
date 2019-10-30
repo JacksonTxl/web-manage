dad7e7d8aa07d11cdf2a78c2876864a92b38cb90
“zlx” <1186641583@qq.com>
2019-10-30 15:03:41 +0800

    fix: 活动报名bug修复

--- src/views/biz-modals/marketing/add-ticket.vue
+++ src/views/biz-modals/marketing/add-ticket.vue
@@ -1,7 +1,7 @@
 <template>
   <st-modal :title="modalTitle" v-model="show" width="500px">
     <div class="modal-marketing-add-ticket">
-      <st-form :form="form" labelWidth="66px">
+      <st-form :form="form">
         <st-form-item label="票种类型" required>
           <a-radio-group
             @change="onChangeGetTicketType"
@@ -188,8 +188,6 @@ export default {
       show: false,
       ticketType: 1,
       isBulk: 1,
-      // TODO: 用户人群暂未开启，前端静态填写 为全部用户 id 为 0
-      crowdIdOptions: [{ label: '全部用户', value: 0 }],
       radioStyle: {
         display: 'block'
       },
@@ -262,13 +260,6 @@ export default {
   },
   methods: {
     moment,
-    getCrowdName(crowdId) {
-      let crowdName = ''
-      this.crowdIdOptions.forEach(item => {
-        crowdName = item.value === crowdId ? item.label : ''
-      })
-      return crowdName
-    },
     formatData(values) {
       const {
         ticket_name,
@@ -320,7 +311,6 @@ export default {
         ticket_remark,
         buy_time
       } = values
-      const crowd_name = this.getCrowdName(crowd_id)
       let buy_start_time = ''
       let buy_end_time = ''
       if (buy_time_limit === 1) {
@@ -334,7 +324,7 @@ export default {
         ticket_name,
         ticket_price,
         ticket_total_num,
-        crowd_name,
+        crowd_id,
         group_buy_min,
         buy_limit_min,
         buy_limit_max,
