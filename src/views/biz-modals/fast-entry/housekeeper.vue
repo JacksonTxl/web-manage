99a1d2c219b88871dae402912d0fefed7b79a082
flowMemory <zhengtengfei@styd.cn>
2019-10-30 19:59:22 +0800

    fix(九宫格): 九宫格云管家修改

--- src/views/biz-modals/fast-entry/housekeeper.vue
+++ src/views/biz-modals/fast-entry/housekeeper.vue
@@ -4,16 +4,23 @@
     class="modal-fast-entry-housekeeper"
     v-model="show"
     :footer="null"
-    width="320px"
+    width="504px"
   >
     <div>
       <div :class="b('wrapper')">
-        <!-- <img :class="b('code-img')" src="@/assets/img/fast-entry/code.png" alt="扫描二维码" /> -->
-        <p class="mg-t16">
-          扫描二维码
-          <br />
-          下载三体云管家App
-        </p>
+        <div :class="b('code_pic')">
+          <img
+            :class="b('code-img')"
+            src="@/assets/img/fast-entry/android.png"
+            alt="扫描二维码"
+          />
+          <img
+            :class="b('code-img')"
+            src="@/assets/img/fast-entry/ios.png"
+            alt="扫描二维码"
+          />
+        </div>
+        <p class="mg-t24">扫描二维码，下载三体云管家App</p>
       </div>
     </div>
   </st-modal>
