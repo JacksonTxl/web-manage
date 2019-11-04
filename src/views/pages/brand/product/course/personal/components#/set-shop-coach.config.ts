17ef35e5d6f6327440ca36043d6160a16d646bf6
liyue <liyue@liyuedeMacBook-Pro.local>
2019-11-04 17:40:31 +0800

    feat: 1002287 课程名称字数限制

--- src/views/pages/brand/product/course/personal/components#/set-shop-coach.config.ts
+++ src/views/pages/brand/product/course/personal/components#/set-shop-coach.config.ts
@@ -9,7 +9,7 @@ export const ruleOptions = (vm: any) => {
         },
         {
           min: 4,
-          message: '支持输入4~30个字的课程名称'
+          message: '支持输入1~30个字的课程名称'
         }
       ]
     },
