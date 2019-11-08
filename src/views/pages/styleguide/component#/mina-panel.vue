bac368a96132c6275b3b0445fab2953428123d67
“zlx” <1186641583@qq.com>
2019-11-08 16:54:48 +0800

    fix: 员工详情页和styleguide优化

---
+++ src/views/pages/styleguide/component#/mina-panel.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="mg-t16 sg-app-content" style="height:800px">
+    <st-mina-panel>
+      <div slot="preview">
+        放手机预览
+        <h5-container></h5-container>
+      </div>
+      这里放预览之外的配置
+    </st-mina-panel>
+  </section>
+</template>
+
+<script>
+import H5Container from '@/views/biz-components/h5/h5-container'
+export default {
+  name: 'StMinaPanelDemo',
+  components: {
+    H5Container
+  }
+}
+</script>
