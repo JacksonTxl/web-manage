8336ae7a9fc7453e7cfb67377f46572c223a4a84
zhengtengfei <zhengtengfei@styd.cn>
2019-11-11 13:55:57 +0800

    fix(1008973): 【UI-私教课】

--- src/views/pages/shop/sold/course/list/personal.vue
+++ src/views/pages/shop/sold/course/list/personal.vue
@@ -19,7 +19,7 @@
         <!-- NOTE: 导出 -->
         <!-- <st-button v-if="auth.export" type="primary">批量导出</st-button> -->
       </div>
-      <div :class="basic('table')">
+      <div>
         <!--
           NOTE: 本期不做，先去掉选择框
           :alertSelection="{onReset: onClear}"
