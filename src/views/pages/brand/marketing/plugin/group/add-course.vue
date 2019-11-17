74b1ba7516294ad06f7334ab570cf247402c459c
liuchaofan <liuchaofan@styd.cn>
2019-11-17 14:54:47 +0800

    feat(brand/group): 新增会籍卡选择逻辑

--- src/views/pages/brand/marketing/plugin/group/add-course.vue
+++ src/views/pages/brand/marketing/plugin/group/add-course.vue
@@ -47,7 +47,10 @@
               <!-- <st-range-picker
                 v-model="decorators.activityTime"
               ></st-range-picker> -->
-              <st-range-picker :value="selectTime"></st-range-picker>
+              <st-range-picker
+                :disabledDays="180"
+                :value="selectTime"
+              ></st-range-picker>
             </st-form-item>
             <st-form-item label="优惠设置" required>
               <div :class="basic('table')">
@@ -149,7 +152,6 @@
 <script>
 import { ruleOptions, cardColumns } from './add-course.config'
 import { AddMemberService } from './add-member.service'
-// import AddMemberService from './add-course.service'
 import moment from 'moment'
 export default {
   serviceInject() {
@@ -197,7 +199,7 @@ export default {
           placeholder: '开始日期',
           disabled: false,
           value: null,
-          format: 'YYYY-MM-DD',
+          format: 'YYYY-MM-DD HH:mm',
           change: $event => {}
         },
         endTime: {
@@ -205,7 +207,7 @@ export default {
           placeholder: '结束日期',
           disabled: false,
           value: null,
-          format: 'YYYY-MM-DD',
+          format: 'YYYY-MM-DD HH:mm',
           change: $event => {},
           disabledDate: this.disabledDate
         }
@@ -234,61 +236,60 @@ export default {
       )
     },
     onSubmit() {
-      this.addMemberService.getMember()
-      // this.form.validate().then(values => {
-      //   console.log(this.editType, 'this.editType')
-      //   let params = {}
-      //   if (this.editType === 1 || this.editType === 3) {
-      //     // 编辑
-      //     params = {
-      //       id: 1,
-      //       product_type: 4,
-      //       activity_name: values.activity_name,
-      //       end_time: this.selectTime.endTime.value,
-      //       stock_total: values.stock_total
-      //     }
-      //   } else {
-      //     let list = []
-      //     this.selectedRowKeys.forEach((id, index) => {
-      //       this.tableData.forEach(item => {
-      //         if (item.id === id) {
-      //           list.push({ sku_id: id, group_price: item.group_price })
-      //         }
-      //       })
-      //     })
-      //     params = {
-      //       product_type: 4, // 会籍卡
-      //       activity_name: values.activity_name, // 活动名称
-      //       product_id: this.cardId, //商品id
-      //       sku: list, //卡、课规格[{“sku_id”:1,”group_price”:20},]
-      //       // start_time: moment(values.start_time).format('YYYY-MM-DD HH:MM'),
-      //       // end_time: moment(values.end_time).format('YYYY-MM-DD HH:MM'),
-      //       start_time: this.selectTime.startTime.value,
-      //       end_time: this.selectTime.endTime.value,
-      //       group_sum: values.group_sum, //成团人数
-      //       valid_time: values.valid_time, //拼团有效期
-      //       is_limit_stock: '', //是否限制库存0不限制 1限制
-      //       stock_total: values.stock_total, //库存
-      //       shop_ids: '', //门店ids [1,2,3,4]
-      //       published_type: '', //发布状态(1-立即发布 2-暂不发布 3-定时发布)
-      //       published_time: '' //发布时间
-      //     }
-      //   }
-      //   if (this.editType) {
-      //     // 编辑 type为2，3只能编辑名称，结束时间，库存
-      //     this.addMemberService.editGroup(params).subscribe(res => {
-      //       console.log(params, res, '这是编辑返回的数据')
-      //     })
-      //   } else {
-      //     this.addMemberService.addGroup(params).subscribe(res => {
-      //       // 新建成功 todo(路由地址未确定)
-      //       console.log(params, res, '==============')
-      //       // this.$router.push({
-      //       //   path: `/brand/marketing/plugin/group/list`
-      //       // })
-      //     })
-      //   }
-      // })
+      this.form.validate().then(values => {
+        console.log(this.editType, 'this.editType')
+        let params = {}
+        if (this.editType === 1 || this.editType === 3) {
+          // 编辑
+          params = {
+            id: 1,
+            product_type: 4,
+            activity_name: values.activity_name,
+            end_time: this.selectTime.endTime.value,
+            stock_total: values.stock_total
+          }
+        } else {
+          let list = []
+          this.selectedRowKeys.forEach((id, index) => {
+            this.tableData.forEach(item => {
+              if (item.id === id) {
+                list.push({ sku_id: id, group_price: item.group_price })
+              }
+            })
+          })
+          params = {
+            product_type: 4, // 会籍卡
+            activity_name: values.activity_name, // 活动名称
+            product_id: this.cardId, //商品id
+            sku: list, //卡、课规格[{“sku_id”:1,”group_price”:20},]
+            // start_time: moment(values.start_time).format('YYYY-MM-DD HH:MM'),
+            // end_time: moment(values.end_time).format('YYYY-MM-DD HH:MM'),
+            start_time: this.selectTime.startTime.value,
+            end_time: this.selectTime.endTime.value,
+            group_sum: values.group_sum, //成团人数
+            valid_time: values.valid_time, //拼团有效期
+            is_limit_stock: '', //是否限制库存0不限制 1限制
+            stock_total: values.stock_total, //库存
+            shop_ids: '', //门店ids [1,2,3,4]
+            published_type: '', //发布状态(1-立即发布 2-暂不发布 3-定时发布)
+            published_time: '' //发布时间
+          }
+        }
+        if (this.editType) {
+          // 编辑 type为2，3只能编辑名称，结束时间，库存
+          this.addMemberService.editGroup(params).subscribe(res => {
+            console.log(params, res, '这是编辑返回的数据')
+          })
+        } else {
+          this.addMemberService.addGroup(params).subscribe(res => {
+            // 新建成功 todo(路由地址未确定)
+            console.log(params, res, '==============')
+            // this.$router.push({
+            //   path: `/brand/marketing/plugin/group/list`
+            // })
+          })
+        }
+      })
     }
   }
 }
