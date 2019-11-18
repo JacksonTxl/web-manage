f82a46afd0145f3271cb85f7992eb33ee9a1b6cf
“zlx” <1186641583@qq.com>
2019-11-18 14:24:26 +0800

    fix: 代码还原

--- src/views/biz-modals/brand/marketing/poster.vue
+++ src/views/biz-modals/brand/marketing/poster.vue
@@ -2,7 +2,10 @@
   <st-modal :title="title" v-model="show" width="400px" :footer="null">
     <div :class="basic()">
       <p>{{ message }}</p>
-      <st-image :src="url" :class="activeClass" />
+      <img v-if="!isLoading" :src="url" :class="activeClass" />
+      <div v-else class="loading-state">
+        <a-spin size="large" />
+      </div>
       <div>
         <st-button
           block
@@ -20,6 +23,7 @@
 </template>
 <script>
 import { PosterService } from './poster.service'
+import { AppConfig } from '@/constants/config'
 import { OssService } from '@/services/oss.service'
 import { ShsService } from '@/services/shs.service'
 import { POSTER } from '@/constants/brand/marketing'
@@ -31,14 +35,31 @@ export default {
   serviceInject() {
     return {
       posterService: PosterService,
+      appConfig: AppConfig,
       shsService: ShsService
     }
   },
   serviceProviders() {
-    return [ShsService, PosterService]
+    return [ShsService]
+  },
+  rxState() {
+    return {
+      loading: this.posterService.loading$,
+      isLoading: this.shsService.loading$,
+      token: this.posterService.token$,
+      info: this.posterService.info$,
+      qrcode: this.posterService.qrcode$
+    }
   },
   props: {
-    shsImageRquerst: {}
+    type: {
+      type: Number,
+      default: 1
+    },
+    id: {
+      type: String,
+      default: '0'
+    }
   },
   data() {
     return {
@@ -53,9 +74,29 @@ export default {
     }
   },
   created() {
-    this.shsService.getShsImage(this.shsImageRquerst).subscribe(res => {
-      this.url = res
-    })
+    if (this.type === this.POSTER.COUPON_TYPE) {
+      this.posterService.getPosterInfo(this.id).subscribe(() => {
+        this.shsService
+          .getShsImage({
+            qrcode_url: this.info.qrcode_url,
+            brand_logo: this.info.brand_logo,
+            brand_name: this.info.brand_name,
+            price: this.info.price
+          })
+          .subscribe(res => {
+            this.url = res
+          })
+      })
+    } else if (this.type === this.POSTER.QRCODE_TYPE) {
+      this.activeClass = 'qrcode'
+      this.title = '小程序码'
+      this.message =
+        '将小程序码贴在宣城刊物、店内等任何地方，用户用手机扫一扫后，即可参与活动转化下单。'
+      this.button = '下载小程序码'
+      this.posterService.getQrcode().subscribe(res => {
+        this.url = res.qrcode
+      })
+    }
   },
 
   methods: {
@@ -65,6 +106,24 @@ export default {
       a.target = '_blank'
       a.download = 'poster.png'
       a.click()
+
+      // let image = new Image()
+      // // 解决跨域 Canvas 污染问题
+      // image.onload = function() {
+      //   let canvas = document.createElement("canvas");
+      //   canvas.width = image.width;
+      //   canvas.height = image.height;
+      //   let context = canvas.getContext("2d");
+      //   context.drawImage(image, 0, 0, image.width, image.height);
+      //   let url = canvas.toDataURL("image/png"); //得到图片的base64编码数据
+      //   console.log('url', url)
+      //   let a = document.createElement("a"); // 生成一个a元素
+      //   let event = new MouseEvent("click"); // 创建一个单击事件
+      //   a.download = "poster.png"; // 设置图片名称
+      //   a.href = url; // 将生成的URL设置为a.href属性
+      //   a.dispatchEvent(event); // 触发a的单击事件
+      // }
+      // image.src = this.url
     }
   }
 }
