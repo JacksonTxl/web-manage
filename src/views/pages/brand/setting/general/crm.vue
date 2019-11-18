de1e2f9fb9a56ed3a609afc8fe820d0387d33e0f
liyue <liyue@liyuedeMacBook-Pro.local>
2019-11-18 14:42:52 +0800

    fix: 表单校验

--- src/views/pages/brand/setting/general/crm.vue
+++ src/views/pages/brand/setting/general/crm.vue
@@ -81,7 +81,9 @@
     <div class="mg-t24">
       <a-row class="align-items-center">
         <a-col :span="20">
-          <st-t4 class="mg-t4">开启销售客户保护</st-t4>
+          <st-t4 class="mg-t4">
+            开启销售客户保护
+          </st-t4>
           <div class="st-des mg-t4">
             客户分配后，可对该跟进人员名下客户进行保护，客保期间其他人员无法跟进
           </div>
@@ -94,7 +96,17 @@
         </a-col>
       </a-row>
       <div :class="bPage('custom')" v-if="crmRule.sales_is_protect">
-        <st-t4 class="mg-t4 mg-b24">销售跟进客户保护天数</st-t4>
+        <st-t4 class="mg-t4 mg-b24">
+          销售跟进客户保护天数
+          <a-tooltip placement="top">
+            <template slot="title">
+              <span>
+                客户保护天数,为员工分配客户后,该员工可跟进会员的天数,超过客保天数后,自动解除跟进人,不限制表示可以一直跟进下去
+              </span>
+            </template>
+            <span><st-icon color="#9bacb9" type="help" /></span>
+          </a-tooltip>
+        </st-t4>
         <a-radio-group v-model="crmRule.sales_is_protect_limit">
           <a-radio :value="0" class="mg-r48">不限制</a-radio>
           <a-radio :value="1">
@@ -153,7 +165,17 @@
         </a-col>
       </a-row>
       <div :class="bPage('custom')" v-if="crmRule.coach_is_protect">
-        <st-t4 class="mg-t4 mg-b24">教练跟进客户保护天数</st-t4>
+        <st-t4 class="mg-t4 mg-b24">
+          教练跟进客户保护天数
+          <a-tooltip placement="top">
+            <template slot="title">
+              <span>
+                客户保护天数,为员工分配客户后,该员工可跟进会员的天数,超过客保天数后,自动解除跟进人,不限制表示可以一直跟进下去
+              </span>
+            </template>
+            <span><st-icon color="#9bacb9" type="help" /></span>
+          </a-tooltip>
+        </st-t4>
         <a-radio-group v-model="crmRule.coach_is_protect_limit">
           <a-radio :value="0" class="mg-r48">不限制</a-radio>
           <a-radio :value="1">
@@ -304,6 +326,72 @@ export default {
       })
     },
     setCrmRule() {
+      if (this.crmRule.sales_is_limit === 0) {
+        this.crmRule.sales_limit_num = ''
+      } else {
+        if (!this.crmRule.sales_limit_num) {
+          this.messageService.warning({
+            content: '请输入大于0的销售可跟进客户天数'
+          })
+          return
+        }
+      }
+      if (this.crmRule.coach_is_limit === 0) {
+        this.crmRule.coach_limit_num = ''
+      } else {
+        if (!this.crmRule.coach_limit_num) {
+          this.messageService.warning({
+            content: '请输入大于0的教练可跟进客户天数'
+          })
+          return
+        }
+      }
+      if (this.crmRule.sales_is_protect_limit === 0) {
+        this.crmRule.sales_protect_days = ''
+      } else {
+        if (!this.crmRule.sales_protect_days) {
+          this.messageService.warning({
+            content: '请输入大于0的销售跟进客户保护天数'
+          })
+          return
+        }
+      }
+      if (this.crmRule.sales_follow_rule !== 3) {
+        this.crmRule.sales_follow_days = ''
+      } else {
+        if (
+          this.crmRule.sales_follow_rule === 3 &&
+          !this.crmRule.sales_follow_days
+        ) {
+          this.messageService.warning({
+            content: '请输入大于0的跟进天数'
+          })
+          return
+        }
+      }
+      if (this.crmRule.coach_is_protect_limit === 0) {
+        this.crmRule.coach_protect_days = ''
+      } else {
+        if (!this.crmRule.coach_protect_days) {
+          this.messageService.warning({
+            content: '请输入大于0的教练跟进客户保护天数'
+          })
+          return
+        }
+      }
+      if (this.crmRule.coach_follow_rule !== 3) {
+        this.crmRule.coach_follow_days = ''
+      } else {
+        if (
+          this.crmRule.coach_follow_rule === 3 &&
+          !this.crmRule.coach_follow_days
+        ) {
+          this.messageService.warning({
+            content: '请输入大于0的跟进天数'
+          })
+          return
+        }
+      }
       return this.crmService.setCrmRule(this.crmRule).subscribe(res => {
         this.messageService.success({ content: '编辑成功' })
         this.getCrmRule()
