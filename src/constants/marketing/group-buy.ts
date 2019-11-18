82e409e38e915e739b4dcb293960e06cb3df286d
ls0713 <mrliu0713@163.com>
2019-11-18 15:41:35 +0800

    feat(constants/group): 增加发布状态枚举

--- src/constants/marketing/group-buy.ts
+++ src/constants/marketing/group-buy.ts
@@ -4,13 +4,13 @@
  * @params {Number} ACTIVITY_STATUS.PUBLISHER 备注：待发布
  * @params {Number} ACTIVITY_STATUS.NO_START 备注：未开始
  * @params {Number} ACTIVITY_STATUS.UNDER_WAY 备注：活动中
- * @params {Number} ACTIVITY_STATUS.UNDER_WAY 备注：已结束
+ * @params {Number} ACTIVITY_STATUS.END 备注：已结束
  * */
 export const enum ACTIVITY_STATUS {
   PUBLISHER = 1,
   NO_START = 2,
   UNDER_WAY = 3,
-  UNDER_WAY = 4
+  END = 4
 }
 
 /**
