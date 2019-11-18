ab8f6888b2c663abc787bf0f6829e91d45917ecf
“zlx” <1186641583@qq.com>
2019-11-18 17:15:31 +0800

    feat: course.service.ts数据统计课程统计代码优化

--- src/views/pages/brand/stat/course.service.ts
+++ src/views/pages/brand/stat/course.service.ts
@@ -26,47 +26,48 @@ export class CourseService {
     private authService: AuthService,
     private userService: UserService
   ) {}
+  SET_SOLD_CHART_DATA(saleCourse: any) {
+    this.soldChartData$.commit(() => {
+      return [
+        {
+          name: '私教课售课',
+          value: saleCourse.sale_personal_num,
+          percent: saleCourse.personal_checkin_percentage
+        },
+        {
+          name: '团体课售课',
+          value: saleCourse.sale_team_num,
+          percent: saleCourse.team_checkin_percentage
+        }
+      ]
+    })
+  }
+  SET_CHECK_IN_CHART_DATA(checkInCourse: any) {
+    this.checkInChartData$.commit(() => [
+      { name: '私教课消课', value: checkInCourse.personal_checkin_num },
+      {
+        name: '团体课消课',
+        value: checkInCourse.team_checkin_num
+      }
+    ])
+  }
+  SET_NOT_CHECK_IN_CHART_DATA(notCheckInCourse: any) {
+    this.notCheckInChartData$.commit(() => [
+      {
+        name: '未私教课消课',
+        value: notCheckInCourse.personal_not_checkin_num
+      },
+      { name: '未团体课消课', value: notCheckInCourse.team_not_checkin_num }
+    ])
+  }
   // 获取营收统计图信息
   getChart(query: CourseChartParams) {
     return this.api.getChart(query).pipe(
       tap(res => {
         const data = res.info
-        const saleCourse = data.sale_course
-        const checkInCourse = data.checkin_course
-        const notCheckInCourse = data.not_checkin_course
-        let soldChartData: any = []
-        let checkInChartData: any = []
-        let notCheckInChartData: any = []
-        soldChartData = [
-          {
-            name: '私教课售课',
-            value: saleCourse.sale_personal_num,
-            percent: saleCourse.personal_checkin_percentage
-          },
-          {
-            name: '团体课售课',
-            value: saleCourse.sale_team_num,
-            percent: saleCourse.team_checkin_percentage
-          }
-        ]
-        checkInChartData = [
-          { name: '私教课消课', value: checkInCourse.personal_checkin_num },
-          {
-            name: '团体课消课',
-            value: checkInCourse.team_checkin_num,
-            percent: '10%'
-          }
-        ]
-        notCheckInChartData = [
-          {
-            name: '未私教课消课',
-            value: notCheckInCourse.personal_not_checkin_num
-          },
-          { name: '未团体课消课', value: notCheckInCourse.team_not_checkin_num }
-        ]
-        this.soldChartData$.commit(() => soldChartData)
-        this.checkInChartData$.commit(() => checkInChartData)
-        this.notCheckInChartData$.commit(() => notCheckInChartData)
+        this.SET_SOLD_CHART_DATA(data.sale_course)
+        this.SET_CHECK_IN_CHART_DATA(data.checkin_course)
+        this.SET_NOT_CHECK_IN_CHART_DATA(data.not_checkin_course)
       })
     )
   }
