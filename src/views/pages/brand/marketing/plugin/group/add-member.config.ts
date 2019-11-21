96043669c5130ffb33c8d11f03bc4f055aed8861
liuxinlin@styd.cn <liuxinlin@styd.cn>
2019-11-21 15:17:09 +0800

    feat(group/group-form): 拼团提出公共config文件

--- src/views/pages/brand/marketing/plugin/group/add-member.config.ts
+++ src/views/pages/brand/marketing/plugin/group/add-member.config.ts
@@ -1,18 +1,8 @@
+import groupFormConfig from './components#/group-form.config'
 export const ruleOptions = (vm: any) => {
-  // const pattern = vm.pattern
-  return {
-    // 活动名称
-    activity_name: {
-      rules: [
-        // {
-        //   validator: (field: any, value: any, values: any) => {
-        //     if (!value || !value.match(pattern.CN_EN_NUM_SPACE('1-30'))) {
-        //       return '请输入会员姓名，支持格式长度1~30中英文'
-        //     }
-        //   }
-        // }
-      ]
-    },
+  const pattern = vm.pattern
+  const groupFrom = groupFormConfig(vm)
+  return Object.assign(groupFrom, {
     // 会籍卡
     cardId: {
       rules: [
@@ -21,71 +11,8 @@ export const ruleOptions = (vm: any) => {
           message: '请选择会籍卡'
         }
       ]
-    },
-    // 参团人数
-    group_sum: {
-      rules: [
-        {
-          validator: (field: any, value: any, values: any) => {
-            if (!value) {
-              return '请填写参团人数'
-            }
-            if (value > 100) {
-              return '参团人数不能超过100，请重新输入'
-            }
-            if (value < 2) {
-              return '参团人数不能少于2，请重新输入'
-            }
-          }
-        }
-      ]
-    },
-    // 拼团有效期
-    valid_time: {
-      rules: [
-        {
-          validator: (field: any, value: any, values: any) => {
-            if (!value) {
-              return '请填写拼团有效期'
-            }
-            if (value > 720) {
-              return '拼团有效期不能超过720，请重新输入'
-            }
-            if (value < 2) {
-              return '拼团有效期不能少于2，请重新输入'
-            }
-          }
-        }
-      ]
-    },
-    // 活动库存
-    stock_total: {
-      rules: [
-        {
-          validator: (field: any, value: any, values: any) => {
-            if (!value) {
-              return '请填写活动库存'
-            }
-            if (value > 999999) {
-              return '活动库存不能超过999999，请重新输入'
-            }
-          }
-        }
-      ]
-    },
-    activity_time: {
-      rules: [
-        {
-          validator: (field: any, value: any, values: any) => {
-            console.log(value)
-            if (!value) {
-              return '请填写活动时间'
-            }
-          }
-        }
-      ]
     }
-  }
+  })
 }
 export const cardColumns = [
   {
