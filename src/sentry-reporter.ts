2429444bb713ec1f883dad4da1723ebf9c3e0977
liuyan <liuyan@styd.cn>
2019-11-21 16:56:26 +0800

    fix: 修改dsn地址

--- src/sentry-reporter.ts
+++ src/sentry-reporter.ts
@@ -6,14 +6,16 @@ const DSN_MAP: any = {
   production: 'https://fe69c74f85df4aa1a85821a81db87d98@ats.styd.cn/19'
 }
 SentryReporter.init({
-  dsn: DSN_MAP[process.env.NODE_ENV],
-  // enable: location.host === 'pro.styd.cn',
-  // 暂时不开启 会造成门店前台储物柜的批量操作弹出模态窗
-  enable: true,
+  dsn: DSN_MAP.production,
+  enable: location.hostname === 'pro.styd.cn',
   // debug: true,
   enviroment: location.host,
   release: process.env.GIT_COMMIT,
   framework: {
     Vue
+  },
+  httpError: {
+    level: 'warning',
+    responseHeaderTags: ['x-request-id']
   }
 })
