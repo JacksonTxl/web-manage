94523f53ce81adb062df7040148f7120a7b381ba
FlynnLee <593029372@qq.com>
2019-11-21 17:05:01 +0800

    fix(sentry): 修正sentry模式

--- src/sentry-reporter.ts
+++ src/sentry-reporter.ts
@@ -2,14 +2,23 @@
 import SentryReporter from 'sentry-reporter-js'
 import Vue from 'vue'
 const DSN_MAP: any = {
-  development: 'https://31ed3f02f8e64e7583e6cab6c946b76a@ats.styd.cn/29',
-  production: 'https://fe69c74f85df4aa1a85821a81db87d98@ats.styd.cn/19'
+  pre: 'https://31ed3f02f8e64e7583e6cab6c946b76a@ats.styd.cn/29',
+  prod: 'https://fe69c74f85df4aa1a85821a81db87d98@ats.styd.cn/19'
 }
+
+let hostEnv = 'local'
+if (location.hostname.includes('pre')) {
+  hostEnv = 'pre'
+}
+if (location.hostname === 'pro.styd.cn') {
+  hostEnv = 'prod'
+}
+
 SentryReporter.init({
-  dsn: DSN_MAP.production,
-  enable: location.hostname === 'pro.styd.cn',
+  dsn: DSN_MAP[hostEnv],
+  enable: hostEnv === 'pre' || hostEnv === 'prod',
   // debug: true,
-  enviroment: location.host,
+  enviroment: location.hostname,
   release: process.env.GIT_COMMIT,
   framework: {
     Vue
