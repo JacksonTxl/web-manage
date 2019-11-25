8c00ab60f5d97c08cc221d9a8a0fcc87d7e5de0f
张飞 <172004133@qq.com>
2019-11-25 16:35:49 +0800

    fix: 1009371 【门店——设置——任务中心】任务明显列表，列表和执行时间显示位置不对

--- src/views/biz-modals/task/detail.config.ts
+++ src/views/biz-modals/task/detail.config.ts
@@ -2,19 +2,23 @@ export const columns = () => {
   return [
     {
       title: '合同编号',
-      dataIndex: 'contract_number'
+      dataIndex: 'contract_number',
+      width: '20%'
     },
     {
       title: '会员姓名',
-      dataIndex: 'member_name'
+      dataIndex: 'member_name',
+      width: '30%'
     },
     {
       title: '类型',
-      dataIndex: 'task_type'
+      dataIndex: 'task_type',
+      width: '10%'
     },
     {
       title: '执行时间',
-      dataIndex: 'task_time'
+      dataIndex: 'task_time',
+      width: '40%'
     }
   ]
 }
