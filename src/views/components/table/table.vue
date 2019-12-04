115f3e548942b8e9a45afa5c374b4fdd0bd2712a
FlynnLee <593029372@qq.com>
2019-12-04 16:55:19 +0800

    fix(components): table add simplePage

--- src/views/components/table/table.vue
+++ src/views/components/table/table.vue
@@ -30,6 +30,10 @@ export default {
     pageMode: {
       type: String,
       default: 'server'
+    },
+    simplePage: {
+      type: Boolean,
+      default: false
     }
   },
   watch: {
@@ -52,6 +56,9 @@ export default {
         emptyText: <st-no-data />
       }
     },
+    defaultPageSize() {
+      return this.simplePage ? 10 : 20
+    },
     tablePagination: {
       get() {
         if (this.pagination === false || this.page === false) {
@@ -60,7 +67,7 @@ export default {
         let _p = merge(
           {
             current: this.current,
-            pageSize: this.pageSize,
+            pageSize: this.defaultPageSize,
             total: this.total,
             showTotal: function(total, range) {
               return `共${total}条`
@@ -77,7 +84,7 @@ export default {
         )
         if (this.page) {
           if (this.page.size) {
-            _p.pageSize = +this.page.size || 20
+            _p.pageSize = +this.page.size || this.defaultPageSize
           }
           if (this.page.current_page) {
             _p.current = +this.page.current_page || 1
@@ -85,7 +92,11 @@ export default {
           if (this.page.total_counts) {
             _p.total = +this.page.total_counts || 0
           }
+          if (this.simplePage) {
+            _p.simple = true
+          }
         }
+        console.log(_p)
         return _p
       }
     }
