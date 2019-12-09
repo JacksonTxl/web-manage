077d0934d86922c3c0a58fb1c0a98ad260489910
吕英杰 <lvyingjie.styd.cn>
2019-12-09 21:16:52 +0800

    feat(shop/stored): 新增云店列表

---
+++ src/views/pages/shop/stored/list.config.ts
@@ -0,0 +1,39 @@
+export const columns = (vm: any) => {
+  return [
+    {
+      title: '商品名称',
+      dataIndex: 'activity_name'
+    },
+    {
+      title: '商品分类',
+      dataIndex: 'product_type'
+    },
+    {
+      title: '销售渠道',
+      dataIndex: 'support_sales'
+    },
+    {
+      title: '上架状态',
+      dataIndex: 'activity_time'
+    },
+    {
+      title: '销量',
+      dataIndex: 'activity_state'
+    },
+    {
+      title: '单价',
+      dataIndex: 'group_total'
+    },
+    {
+      title: '库存',
+      dataIndex: 'group_success_total'
+    },
+    {
+      title: '操作',
+      dataIndex: 'action',
+      width: 140,
+      fixed: 'right',
+      scopedSlots: { customRender: 'action' }
+    }
+  ]
+}
