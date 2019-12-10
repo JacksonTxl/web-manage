7bca64085f5ea100e11b81f3b36b99846f4bbd95
liuyan <liuyan@styd.cn>
2019-12-10 19:13:47 +0800

    feat(1005623): 【品牌-营销插件】创建活动报名，富文本框添加插入图片功能

--- src/views/biz-components/editor/editor-image.vue
+++ src/views/biz-components/editor/editor-image.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="tox-toolbar__group editor__upload">
-    <a-upload :fileList="[]" :customRequest="upload">
+    <a-upload
+      :fileList="[]"
+      :customRequest="upload"
+      :beforeUpload="beforeUpload"
+    >
       <a-icon type="upload" />
     </a-upload>
   </div>
@@ -8,11 +12,13 @@
 
 <script>
 import { OssService } from '../../../services/oss.service'
+import { MessageService } from '@/services/message.service'
 export default {
   name: 'EditImage',
   serviceInject() {
     return {
-      oss: OssService
+      oss: OssService,
+      messageService: MessageService
     }
   },
   data() {
@@ -35,6 +41,13 @@ export default {
         .subscribe(val => {
           this.$emit('change', { image: val, editor: this.editor })
         })
+    },
+    beforeUpload(file) {
+      const isLt2M = file.size / 1024 / 1024 < 2
+      if (!isLt2M) {
+        this.messageService.error({ content: '上传图片最大不能超过2M' })
+      }
+      return isLt2M
     }
   }
 }
