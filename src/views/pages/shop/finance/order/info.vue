e6d1773cf08704b1ac03e1adf496c1a5d92d0b7d
zhengtengfei <zhengtengfei@styd.cn>
2019-12-10 14:17:45 +0800

    fix(1004839): 【SaaS-财务】小班课订单

--- src/views/pages/shop/finance/order/info.vue
+++ src/views/pages/shop/finance/order/info.vue
@@ -37,6 +37,7 @@
             <st-info-item label="购买会员">
               {{ info.member_name }} {{ info.member_mobile }}
             </st-info-item>
+            <st-info-item label="家长">{{ infoParents }}</st-info-item>
             <st-info-item label="下单人">{{ info.operator_name }}</st-info-item>
             <st-info-item label="下单时间">
               {{ info.created_time }}
@@ -115,7 +116,17 @@ export default {
       ORDER_PRODUCT_TYPE
     }
   },
-  computed: {},
+  computed: {
+    infoParents() {
+      const parentsMess = ''
+      if (typeof this.info.parents === object) {
+        parentsMess = `${this.info.parents.name} (${this.info.parents.mobile})`
+      } else {
+        parentsMess = this.info.parents
+      }
+      return parentsMess
+    }
+  },
   methods: {
     // 订单收款modal
     createdOrderPay() {
