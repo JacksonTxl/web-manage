71949968505498015a3ef74ef2554c6d45f17100
崔庆 <cuiqing@styd.cn>
2019-12-16 19:07:26 +0800

    feat(shop/stored): 使用云店概览根据mock数据调接口

--- src/api/v1/shop/store/store.ts
+++ src/api/v1/shop/store/store.ts
@@ -1,10 +1,22 @@
 import { Api } from '@/api/api'
 
+export interface DtoreBoard {
+  date_type: string
+  date: string
+}
 export class StoreApi extends Api {
   /**
    * 数据概况
    */
   dataProfile() {
     return this.http.get('/v1/store/data_profile')
+  }
+  /**
+   * 整体看板
+   */
+  storeBoard(params: DtoreBoard) {
+    return this.http.get('/v1/store/board', {
+      params
+    })
   }
 }
