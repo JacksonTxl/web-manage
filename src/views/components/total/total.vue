38b06f9fd1fb424c302084c3b15da49161535f47
“zlx” <1186641583@qq.com>
2019-12-16 16:01:53 +0800

    fix: 公共组件优化

--- src/views/components/total/total.vue
+++ src/views/components/total/total.vue
@@ -9,6 +9,7 @@
     <div :class="[b('content'), !hasTitle ? b('content--full') : '']">
       <div
         v-if="showData.length > 6"
+        :class="prevClass"
         class="swiper-total-button-prev swiper-button-prev"
         slot="button-prev"
       >
@@ -34,6 +35,7 @@
 
       <div
         v-if="showData.length > 6"
+        :class="nextClass"
         class="swiper-total-button-next swiper-button-next"
         slot="button-next"
       >
@@ -46,6 +48,7 @@
 <script>
 import { swiper, swiperSlide } from 'vue-awesome-swiper'
 import 'swiper/dist/css/swiper.css'
+import moment from 'moment'
 export default {
   name: 'StTotal',
   bem: {
@@ -85,16 +88,26 @@ export default {
         })
       }
       return templateData
-    }
-  },
-  data() {
-    return {
-      sliderOptions: {
+    },
+    timestamp() {
+      return moment().valueOf()
+    },
+    prevClass() {
+      return `swiper-${this.timestamp}-button-next`
+    },
+    nextClass() {
+      return `swiper-${this.timestamp}-button-prev`
+    },
+    navigationButtons() {
+      return {
+        nextEl: `.${this.nextClass}`,
+        prevEl: `.${this.prevClass}`
+      }
+    },
+    sliderOptions() {
+      return {
         autoplay: false,
-        navigation: {
-          nextEl: '.swiper-total-button-next',
-          prevEl: '.swiper-total-button-prev'
-        },
+        navigation: { ...this.navigationButtons },
         // Default parameters
         slidesPerView: 6,
         breakpoints: {
