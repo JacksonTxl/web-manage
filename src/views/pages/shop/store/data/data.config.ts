45687ec6251402d07c428ee6f5be1f8f17113ac8
崔庆 <cuiqing@styd.cn>
2019-12-16 11:30:30 +0800

    feat(shop/stored): 云店数据样式修改以及config.ts添加

---
+++ src/views/pages/shop/store/data/data.config.ts
@@ -0,0 +1,125 @@
+import IconStoredOrder from '@/assets/img/shop/stored/data/icon-stored-order.png'
+import IconStoredTransaction from '@/assets/img/shop/stored/data/icon-stored-transaction.png'
+import IconStoredRevenue from '@/assets/img/shop/stored/data/icon-stored-revenue.png'
+import IconStoredPassenger from '@/assets/img/shop/stored/data/icon-stored-passenger.png'
+import topIconUser from '@/assets/img/shop/stored/data/icon_top_revenue.png'
+import topIconOrder from '@/assets/img/shop/stored/data/icon_top_order.png'
+import topIconGuest from '@/assets/img/shop/stored/data/icon_top_guest.png'
+import topIconMember from '@/assets/img/shop/stored/data/icon_top_member.png'
+
+export const headerInfo = [
+  {
+    icon: topIconUser,
+    title: '营收金额(元)',
+    sum: 8888.888,
+    yesterday: 8888.888
+  },
+  {
+    icon: topIconOrder,
+    title: '订单数(单)',
+    sum: 8888,
+    yesterday: 8888.888
+  },
+  {
+    icon: topIconMember,
+    title: '营收金额(元)',
+    sum: 8888.888,
+    yesterday: 8888.888
+  },
+  {
+    icon: topIconGuest,
+    title: '客单价(元)',
+    sum: 8888.888,
+    yesterday: 8888.888
+  }
+]
+export const wholeNav = [
+  {
+    icon: IconStoredRevenue,
+    title: '营收金额(元)',
+    num: '1,122'
+  },
+  {
+    icon: IconStoredOrder,
+    title: '订单数(单)',
+    num: '1,122'
+  },
+  {
+    icon: IconStoredTransaction,
+    title: '交易会员数(人)',
+    num: '1,122'
+  },
+  {
+    icon: IconStoredPassenger,
+    title: '客单价(元)',
+    num: '1,122'
+  }
+]
+export const dataRing = [
+  { name: '商家后台', value: '2020.0' },
+  { name: '小程序', value: '200' }
+]
+export const dataRings = [
+  { name: '潜在会员', value: '2020.0' },
+  { name: '正式会员', value: '200' }
+]
+export const dataRingss = [
+  { name: '潜在会员1', value: '2020.0' },
+  { name: '潜在会员2', value: '2020.0' },
+  { name: '潜在会员3', value: '2020.0' },
+  { name: '潜在会员4', value: '2020.0' },
+  { name: '潜在会员5', value: '2020.0' },
+  { name: '潜在会员6', value: '2020.0' },
+  { name: '潜在会员7', value: '2020.0' },
+  { name: '潜在会员8', value: '2020.0' },
+  { name: '潜在会员9', value: '2020.0' },
+  { name: '正式会员', value: '200' }
+]
+export const salesList = {
+  title: ['排名', '商品', '销量(件)'],
+  data: [
+    {
+      age: 18,
+      sex: '女'
+    },
+    {
+      age: 18,
+      sex: '男'
+    },
+    {
+      age: 18,
+      sex: '男'
+    },
+    {
+      age: 18,
+      sex: '男'
+    },
+    {
+      name: 1,
+      age: 1,
+      sex: 1
+    }
+  ]
+}
+export const courseDaily = [
+  {
+    date: '12-03',
+    总营收: 10
+  },
+  {
+    date: '12-04',
+    总营收: 0
+  },
+  {
+    date: '12-08',
+    总营收: 0
+  },
+  {
+    date: '12-09',
+    总营收: 0
+  },
+  {
+    date: '12-10',
+    总营收: 0
+  }
+]
