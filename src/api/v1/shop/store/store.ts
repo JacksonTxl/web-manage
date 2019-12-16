04e0bdeff10d970d2f10a1b15ee418a58813e61d
吕英杰 <lvyingjie.styd.cn>
2019-12-16 17:55:57 +0800

    feat(shop/store): mock数据测试

--- src/api/v1/shop/store/store.ts
+++ src/api/v1/shop/store/store.ts
@@ -1,10 +1,20 @@
 import { Api } from '@/api/api'
-
+export interface StoreList {
+  category_id: number
+  product_name: string
+  shelves_status: number
+}
 export class StoreApi extends Api {
   /**
    * 数据概况
    */
   dataProfile() {
     return this.http.get('/v1/store/data_profile')
+  }
+  /**
+   * 商品列表
+   */
+  getList(query: StoreList) {
+    return this.http.get(`/v1/store/product/1`, { query })
   }
 }
