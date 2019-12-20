e053f70df2d44943c95870b74c162fd88a9d4654
张飞 <172004133@qq.com>
2019-12-20 14:13:49 +0800

    fix: 支持入场人数的tooltip提示修改

--- src/views/pages/brand/product/card/member/period/add.vue
+++ src/views/pages/brand/product/card/member/period/add.vue
@@ -47,7 +47,7 @@
             >
               <template slot="label">
                 支持入场人数
-                <st-help-tooltip id="TBMCDC001" />
+                <st-help-tooltip id="TBMCDC003" />
               </template>
               <a-select
                 v-decorator="decorators.cardData.support_member_num"
