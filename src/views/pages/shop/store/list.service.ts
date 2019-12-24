c9be1269506c6c068231015667536db3dabe8fc5
吕英杰 <lvyingjie.styd.cn>
2019-12-24 15:25:11 +0800

    feat(shop/store): 商品列表上下架删除功能

--- src/views/pages/shop/store/list.service.ts
+++ src/views/pages/shop/store/list.service.ts
@@ -3,12 +3,14 @@ import { State, Effect } from 'rx-state'
 import { StoreApi, StoreList } from '@/api/v1/shop/store/store'
 import { tap } from 'rxjs/operators'
 import { AuthService } from '@/services/auth.service'
+import { forkJoin } from 'rxjs'
 @Injectable()
 export class ListService implements Controller {
   list$ = new State([])
   page$ = new State({})
   loading$ = new State({})
   info$ = new State({})
+  goodsList$ = new State({})
   // auth$ = this.authService.authMap$({
   //   // 记得设置鉴权
   //   add: 'brand:activity:group_buy|add'
@@ -26,6 +28,15 @@ export class ListService implements Controller {
       })
     )
   }
+  @Effect()
+  getGoodsCategory() {
+    return this.storeApi.getCategory().pipe(
+      tap((res: any) => {
+        console.log(res)
+        this.goodsList$.commit(() => res.list)
+      })
+    )
+  }
   delproduct(id: number) {
     return this.storeApi.delProduct(id).pipe(tap((res: any) => {}))
   }
@@ -39,7 +50,10 @@ export class ListService implements Controller {
   //     })
   //   )
   // }
+  init(params: any) {
+    return forkJoin(this.getList(params), this.getGoodsCategory())
+  }
   beforeEach(to: ServiceRoute, from: ServiceRoute) {
-    return this.getList(to.meta.query)
+    return this.init(to.meta.query)
   }
 }
