50e19ef9056919dc6ecbc90e103369575dd0ded0
liuchaofan <liuchaofan@styd.cn>
2019-12-24 18:53:47 +0800

    feat(shop/store): 修改库存管理

--- src/views/pages/shop/store/stock/list/detail.config.ts
+++ src/views/pages/shop/store/stock/list/detail.config.ts
@@ -1,42 +1,44 @@
 export const detailColumns = [
   {
     title: '商品名称',
-    dataIndex: 'spec',
+    dataIndex: 'product_name',
     align: 'center'
   },
   {
     title: '商品分类',
-    dataIndex: 'price',
+    dataIndex: 'category_name',
     align: 'center'
   },
   {
     title: '出入库类型',
-    dataIndex: 'price2',
-    align: 'center'
+    dataIndex: 'stock_flow',
+    align: 'center',
+    slots: { title: 'stock_flow' },
+    scopedSlots: { customRender: 'stock_flow' }
   },
   {
     title: '出入库',
-    dataIndex: 'price3',
+    dataIndex: 'change_amount',
     align: 'center'
   },
   {
     title: '剩余量',
-    dataIndex: 'price4',
+    dataIndex: 'surplus_amount',
     align: 'center'
   },
   {
     title: '操作时间',
-    dataIndex: 'price5',
+    dataIndex: 'operator_time',
     align: 'center'
   },
   {
     title: '操作人',
-    dataIndex: 'price7',
+    dataIndex: 'operator_name',
     align: 'center'
   },
   {
     title: '备注',
-    dataIndex: 'price8',
+    dataIndex: 'remark',
     align: 'center'
   }
 ]
