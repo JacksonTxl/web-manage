8c8cf65656cccd8438c0b07a2994e9643cbce6b4
“zlx” <1186641583@qq.com>
2019-12-26 17:18:19 +0800

    fix: 报表统计

--- src/views/pages/brand/stat/order.service.ts
+++ src/views/pages/brand/stat/order.service.ts
@@ -27,13 +27,14 @@ export class OrderService implements Controller {
   ) {}
   // 获取营收统计图信息
   getChart(query: OrderChartParams) {
+    const small_course = this.userService.c('small_course')
     return this.orderApi.getChart(query).pipe(
       tap(res => {
         const data = res.info
         const arr = [
           { group: '云店', 成单数量: data.shop.num, 客单价: data.shop.avg },
           {
-            group: this.userService.c('small_course'),
+            group: small_course,
             成单数量: data.small_course.num,
             客单价: data.small_course.avg
           },
