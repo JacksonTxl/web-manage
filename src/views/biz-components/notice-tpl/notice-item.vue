2ce84a92e8883cf54532169a785750f5182f6f57
李月 <liyue@liyuedeMacBook-Pro.local>
2020-01-09 11:27:13 +0800

    fix: 找回丢失代码

--- src/views/biz-components/notice-tpl/notice-item.vue
+++ src/views/biz-components/notice-tpl/notice-item.vue
@@ -139,6 +139,12 @@
                 >
                   {{ params.course_type.personal_course.name }}
                 </st-checkbox>
+                <st-checkbox
+                  v-if="params.course_type.small_course"
+                  v-model="params.course_type.small_course.value"
+                >
+                  {{ params.course_type.small_course.name }}
+                </st-checkbox>
               </span>
             </div>
             <div class="mg-b16" v-if="Object.keys(info.receiver).length > 0">
@@ -343,6 +349,10 @@ export default {
           personal_course: {
             value: 0,
             name: '私教课'
+          },
+          small_course: {
+            value: 0,
+            name: '小班课'
           }
         },
         order_type: {
@@ -448,7 +458,9 @@ export default {
       if (this.info.course_type.personal_course) {
         course_type.personal_course = 0
       }
-
+      if (this.info.course_type.small_course) {
+        course_type.small_course = 0
+      }
       if (this.info.order_type.advance) {
         order_type.advance = 0
       }
@@ -482,6 +494,11 @@ export default {
       if (this.info.course_type.personal_course) {
         course_type.personal_course = this.params.course_type.personal_course
           .value
+          ? 1
+          : 0
+      }
+      if (this.info.course_type.small_course) {
+        course_type.small_course = this.params.course_type.small_course.value
           ? 1
           : 0
       }
