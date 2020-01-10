d423bcb26feb9813338c48a9017bc8ef4cfc49db
超凡 <353985744@qq.com>
2020-01-10 18:43:43 +0800

    feat(shop): 修改课程排期回退问题

--- src/views/pages/shop/product/course/schedule/personal-team/personal-team.vue
+++ src/views/pages/shop/product/course/schedule/personal-team/personal-team.vue
@@ -7,6 +7,7 @@
       :cardList="cardList"
       :startDate="startDate"
       fixed
+      ref="calendar"
     >
       <div slot="toolbar-left">
         <st-button
@@ -126,6 +127,14 @@ export default {
     // 刷新页面
     onScheduleChange() {
       this.$router.push({ query: this.$searchQuery })
+    }
+  },
+  watch: {
+    $route(newValue, oldValue) {
+      this.service.scheduleService.getList(this.$searchQuery)
+      this.$refs.calendar.getWeeks(
+        this.$searchQuery.data_type === 'week' ? 'week' : ''
+      )
     }
   }
 }
