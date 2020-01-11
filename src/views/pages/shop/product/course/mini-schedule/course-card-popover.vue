0c01aa2ce9b17157e4aa2bec0eca67cab021a552
zhengtengfei <zhengtengfei@styd.cn>
2020-01-11 13:50:21 +0800

    fix(小班课): 增加冲突列表的最大高度

--- src/views/pages/shop/product/course/mini-schedule/course-card-popover.vue
+++ src/views/pages/shop/product/course/mini-schedule/course-card-popover.vue
@@ -11,7 +11,7 @@
         <st-table
           :pagination="false"
           :columns="columns"
-          :scroll="{ x: 680 }"
+          :scroll="{ x: 680, y: 500 }"
           :dataSource="cardItem.conflictList"
           rowKey="index"
         ></st-table>
