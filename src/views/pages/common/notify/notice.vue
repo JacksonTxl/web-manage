e997bb3e2cdd421cbc633c975fd4c327d89df9f8
郑林雄 <1186641583@qq.com>
2020-01-14 18:37:17 +0800

    fix: 消息通知查看详情修改

--- src/views/pages/common/notify/notice.vue
+++ src/views/pages/common/notify/notice.vue
@@ -82,7 +82,7 @@ export default {
           if (link.length > 0) {
             // 有链接链接弹窗
             window.open(link)
-          } else if (info.image_key.length > 0 && info.notify_type.id === 2) {
+          } else if (info.image_key.length > 0 && notify_type.id === 2) {
             // 有图片没连接出现图片
             const list = info.image_key.map(item => {
               return {
